60ec1c2ac361ac0fa0fd478c0701fcb2ac4fcf7a
Bryan Loh <lamofacade@gmail.com>
2021-02-22 01:57:37 +0800

    Remove redundant build scripts in package.json and suppressed MISSING_NAME_OPTION_FOR_IIFE_EXPORT rollup warning

--- build/contents/Repeat.js
+++ build/contents/Repeat.js
@@ -1,27 +1,12 @@
 (function (React) {
-  'use strict';
+    'use strict';
 
-  function _interopDefaultLegacy(e) {
-    return e && typeof e === 'object' && 'default' in e ? e : { default: e };
-  }
+    function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
-  var React__default = /*#__PURE__*/ _interopDefaultLegacy(React);
+    var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
 
-  var index = function index(props) {
-    return React__default['default'].createElement(
-      'div',
-      null,
-      'This is spawned from the repeat package'
-    );
-  };
-  var index$1 = {
-    toSpawn: function () {
-      return true;
-    },
-    body: index,
-    label: 'Repeat Test Tab',
-    iconName: 'build',
-  };
+    var index=function index(props){return React__default['default'].createElement("div",null,"This is spawned from the repeat package")};var index$1 = {toSpawn:function(){return true},body:index,label:"Repeat Test Tab",iconName:"build"};
 
-  return index$1;
-})(React);
+    return index$1;
+
+}(React));
