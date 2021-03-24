63cf9bfd9d66e49e42006476f8b703b115aa0278
Anonymxtrix <Anonymxtrix@users.noreply.github.com>
2021-03-24 17:57:47 +0000

    deploy: ed92945682120f310b3443b6d5caa331faa6d2c0

--- bundles/repeat.js
+++ bundles/repeat.js
@@ -1,6 +1,18 @@
 (function () {
     'use strict';
 
+    (function() {
+        const env = {};
+        try {
+            if (process) {
+                process.env = Object.assign({}, process.env);
+                Object.assign(process.env, env);
+                return;
+            }
+        } catch (e) {} // avoid ReferenceError: process is not defined
+        globalThis.process = { env:env };
+    })();
+
     function repeat(f,n){return n===0?function(x){return x}:function(x){return f(repeat(f,n-1)(x))}}function twice(f){return repeat(f,2)}function thrice(f){return repeat(f,3)}var index = (function(){return {repeat:repeat,twice:twice,thrice:thrice}});
 
     return index;
--- tabs/Repeat.js
+++ tabs/Repeat.js
@@ -5,6 +5,18 @@
 
     var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
 
+    (function() {
+        const env = {};
+        try {
+            if (process) {
+                process.env = Object.assign({}, process.env);
+                Object.assign(process.env, env);
+                return;
+            }
+        } catch (e) {} // avoid ReferenceError: process is not defined
+        globalThis.process = { env:env };
+    })();
+
     /*! *****************************************************************************
     Copyright (c) Microsoft Corporation.
 
