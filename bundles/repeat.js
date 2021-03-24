2588ed45b4c2f9f0ee3ce6ead0717a383f055f07
MarcusTXK <MarcusTXK@users.noreply.github.com>
2021-03-24 09:55:25 +0000

    deploy: 69915ea9a1b77d33e712dec6c4b6eaba443f05db

--- bundles/repeat.js
+++ bundles/repeat.js
@@ -1,8 +1,8 @@
 (function () {
     'use strict';
 
-    function _repeat(f,n){return n===0?function(x){return x}:function(x){return f(_repeat(f,n-1)(x))}}function twice(f){return _repeat(f,2)}function thrice(f){return _repeat(f,3)}function repeat(){return {repeat:_repeat,twice:twice,thrice:thrice}}
+    function repeat(f,n){return n===0?function(x){return x}:function(x){return f(repeat(f,n-1)(x))}}function twice(f){return repeat(f,2)}function thrice(f){return repeat(f,3)}var index = (function(){return {repeat:repeat,twice:twice,thrice:thrice}});
 
-    return repeat;
+    return index;
 
 }());
