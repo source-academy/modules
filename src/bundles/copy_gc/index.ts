65a5fae37bca1c55d37c96b3ce94d4e6bc8af691
violinyap <violinyap@gmail.com>
2021-04-08 10:09:47 +0800

    add copy func

--- src/bundles/copy_gc/index.ts
+++ src/bundles/copy_gc/index.ts
@@ -5,35 +5,44 @@ import { MemoryHeaps, Memory, Tag } from './types';
 
 // Global Variables
 let ROW: number = 10;
-let COLUMN: number = 32;
+const COLUMN: number = 32;
 let MEMORY_SIZE: number = -99;
 let TO_SPACE: number;
 let FROM_SPACE: number;
 let memory: Memory;
 // eslint-disable-next-line prefer-const
 let memoryHeaps: Memory[] = [];
+// eslint-disable-next-line prefer-const
+let commandHeap: any[] = [];
+// eslint-disable-next-line prefer-const
+let operationHeaps: string[] = [];
 let toMemoryMatrix: number[][];
 let fromMemoryMatrix: number[][];
 let tags: Tag[];
 let typeTag: string[];
 // eslint-disable-next-line prefer-const
 let flips: number[] = [];
+let TAG_SLOT: number = 0;
+let SIZE_SLOT: number = 1;
+let FIRST_CHILD_SLOT: number = 2;
+let LAST_CHILD_SLOT: number = 3;
 
 function initialise_tag(allTag: number[], types: string[]): void {
   tags = allTag;
   typeTag = types;
 }
 
-function update(toSpace: number, fromSpace: number): void {
-  TO_SPACE = toSpace;
-  FROM_SPACE = fromSpace;
-  console.log('updating space size ', TO_SPACE, ' ', FROM_SPACE);
-}
-
-function updateMemoryHeap(newHeap: number[]): void {
+function updateMemoryHeap(ops: string, newHeap: number[]): void {
   if (newHeap) {
+    memory = [];
+    for (let j = 0; j < newHeap.length; j += 1) {
+      // eslint-disable-next-line no-param-reassign
+      memory.push(newHeap[j]);
+    }
     console.log('add new ', memoryHeaps.length, ' -- ', newHeap);
-    memoryHeaps.push(newHeap);
+    memoryHeaps.push(memory);
+    operationHeaps.push(ops);
+    // heapStates.push(memoryHeaps);
   }
 }
 
@@ -42,14 +51,14 @@ function allHeap(newHeap: number[][]): void {
 }
 
 function updateFlip(): void {
-  flips.push(memoryHeaps.length - 1);
+  flips.push(commandHeap.length - 1);
 }
 
 function generateMemory(): void {
   toMemoryMatrix = [];
   for (let i = 0; i < ROW / 2; i += 1) {
     memory = [];
-    for (let j = 0; j < COLUMN && i * COLUMN + j < MEMORY_SIZE; j += 1) {
+    for (let j = 0; j < COLUMN && i * COLUMN + j < MEMORY_SIZE / 2; j += 1) {
       // eslint-disable-next-line no-param-reassign
       memory.push(i * COLUMN + j);
     }
@@ -65,14 +74,117 @@ function generateMemory(): void {
     }
     fromMemoryMatrix.push(memory);
   }
+
+  const obj = {
+    type: 'init',
+    to: TO_SPACE,
+    from: FROM_SPACE,
+    heap: [],
+    left: -1,
+    right: -1,
+  };
+
+  commandHeap.push(obj);
 }
 
 function initialise_memory(memorySize: number): void {
   MEMORY_SIZE = memorySize;
-  COLUMN = 32;
   ROW = MEMORY_SIZE / COLUMN;
   generateMemory();
   console.log('updating memory size ', MEMORY_SIZE);
+}
+
+function newCommand(type, toSpace, fromSpace, left, right, heap): void {
+  const newType = type;
+  const newToSpace = toSpace;
+  const newFromSpace = fromSpace;
+  const newLeft = left;
+  const newRight = right;
+  memory = [];
+  for (let j = 0; j < heap.length; j += 1) {
+    // eslint-disable-next-line no-param-reassign
+    memory.push(heap[j]);
+  }
+
+  const obj = {
+    type: newType,
+    to: newToSpace,
+    from: newFromSpace,
+    heap: memory,
+    left: newLeft,
+    right: newRight,
+  };
+
+  commandHeap.push(obj);
+}
+
+function newCopy(left, right, heap): void {
+  const { length } = commandHeap;
+  const toSpace = commandHeap[length - 1].to;
+  const fromSpace = commandHeap[length - 1].from;
+
+  newCommand('Copy', toSpace, fromSpace, left, right, heap);
+}
+
+function newFlip(left, right, heap): void {
+  const { length } = commandHeap;
+  const fromSpace = commandHeap[length - 1].to;
+  const toSpace = commandHeap[length - 1].from;
+
+  newCommand('Flip', toSpace, fromSpace, left, right, heap);
+  updateFlip();
+}
+
+function newPush(left, right, heap): void {
+  const { length } = commandHeap;
+  const toSpace = commandHeap[length - 1].to;
+  const fromSpace = commandHeap[length - 1].from;
+  newCommand('Push OS', toSpace, fromSpace, left, right, heap);
+}
+
+function newPop(res, left, heap): void {
+  const { length } = commandHeap;
+  const toSpace = commandHeap[length - 1].to;
+  const fromSpace = commandHeap[length - 1].from;
+  const newRes = res;
+  const comment: String = `Pop OS (RES = ${newRes} )`;
+  newCommand(comment, toSpace, fromSpace, left, -1, heap);
+}
+
+function newAssign(res, left, heap): void {
+  const { length } = commandHeap;
+  const toSpace = commandHeap[length - 1].to;
+  const fromSpace = commandHeap[length - 1].from;
+  const newRes = res;
+  const comment = `Assign [${left}] = ${newRes} )`;
+  newCommand(comment, toSpace, fromSpace, left, -1, heap);
+}
+
+function newNew(left, right, heap): void {
+  const { length } = commandHeap;
+  const toSpace = commandHeap[length - 1].to;
+  const fromSpace = commandHeap[length - 1].from;
+  newCommand('New', toSpace, fromSpace, left, right, heap);
+}
+
+function updateSlotSegment(
+  tag: number,
+  size: number,
+  first: number,
+  last: number
+): void {
+  if (tag >= 0) {
+    TAG_SLOT = tag;
+  }
+  if (size >= 0) {
+    SIZE_SLOT = size;
+  }
+  if (first >= 0) {
+    FIRST_CHILD_SLOT = first;
+  }
+  if (last >= 0) {
+    LAST_CHILD_SLOT = last;
+  }
 }
 
 function get_memory_size(): number {
@@ -81,6 +193,10 @@ function get_memory_size(): number {
 
 function get_tags(): Tag[] {
   return tags;
+}
+
+function get_command(): any[] {
+  return commandHeap;
 }
 
 function get_flips(): number[] {
@@ -105,6 +221,10 @@ function get_to_memory_matrix(): MemoryHeaps {
 
 function get_from_memory_matrix(): MemoryHeaps {
   return fromMemoryMatrix;
+}
+
+function get_slots(): number[] {
+  return [TAG_SLOT, SIZE_SLOT, FIRST_CHILD_SLOT, LAST_CHILD_SLOT];
 }
 
 function get_to_space(): number {
@@ -133,25 +253,28 @@ function init() {
     get_from_memory_matrix,
     get_to_memory_matrix,
     get_flips,
+    get_slots,
+    get_command,
   };
 }
 
 export default function copy_gc() {
   return {
     init,
-    // get data
-    get_memory_size,
-    get_to_space,
-    get_from_space,
-    get_memory_heap,
     // initialisation
     initialise_memory,
     initialise_tag,
-    update,
     updateMemoryHeap,
     generateMemory,
-    updateFlip,
     allHeap,
+    updateSlotSegment,
+    newCommand,
+    newCopy,
+    newFlip,
+    newPush,
+    newPop,
+    newAssign,
+    newNew,
   };
 }
 
@@ -179,4 +302,12 @@ updateMemoryHeap([20,5,65,23]);
 initialise_tag([53,23], ['hallo', 'int']);
 
 init();
+
+
+// general node layout
+const TAG_SLOT = 0;
+const SIZE_SLOT = 1;
+const FIRST_CHILD_SLOT = 2;
+const LAST_CHILD_SLOT = 3;
+
 */
--- src/tabs/CopyGc/index.tsx
+++ src/tabs/CopyGc/index.tsx
@@ -16,14 +16,17 @@ type State = {
   row: number;
   tags: number[];
   heap: number[];
-  memoryHeap: number[][];
+  commandHeap: any[];
+  command: String;
   flips: number[];
   functionLength: number;
   toMemoryMatrix: number[][];
   fromMemoryMatrix: number[][];
-  content: number;
+  firstChild: number;
+  lastChild: number;
 };
 
+const SIZE_SLOT = 1;
 class CopyGC extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
@@ -39,12 +42,14 @@ class CopyGC extends React.Component<Props, State> {
       row: 0,
       tags: [],
       heap: [],
-      memoryHeap: [],
+      commandHeap: [],
       // eslint-disable-next-line react/no-unused-state
       flips: [0],
       toMemoryMatrix: [],
       fromMemoryMatrix: [],
-      content: -1,
+      firstChild: -1,
+      lastChild: -1,
+      command: '',
     };
   }
 
@@ -56,20 +61,31 @@ class CopyGC extends React.Component<Props, State> {
     const { debuggerContext } = this.props;
     const functions = debuggerContext.result.value;
     const memorySize = functions.get_memory_size();
-    const toSpace = functions.get_to_space();
-    const fromSpace = functions.get_from_space();
     const column = functions.get_column_size();
     const row = functions.get_row_size();
     const tags = functions.get_tags();
-    const memoryHeap = functions.get_memory_heap();
-    // eslint-disable-next-line react/destructuring-assignment
-    const heap = memoryHeap[this.state.value];
+    const commandHeap = functions.get_command();
+    let heap = [];
+    let toSpace = -1;
+    let fromSpace = -1;
+    let command = '';
+    let firstChild = -1;
+    let lastChild = -1;
+
+    if (commandHeap[0]) {
+      const currentHeap = commandHeap[0];
+      heap = currentHeap.heap;
+      toSpace = currentHeap.to;
+      fromSpace = currentHeap.from;
+      command = currentHeap.type;
+      firstChild = currentHeap.left;
+      lastChild = currentHeap.right;
+    }
+
     const toMemoryMatrix = functions.get_to_memory_matrix();
     const fromMemoryMatrix = functions.get_from_memory_matrix();
-    const functionLength = memoryHeap.length;
+    const functionLength = commandHeap.length;
     const flips = functions.get_flips();
-
-    console.log(memoryHeap);
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, arrow-body-style
     this.setState((state: State) => {
@@ -81,45 +97,71 @@ class CopyGC extends React.Component<Props, State> {
         row,
         tags,
         heap,
-        memoryHeap,
         functionLength,
         toMemoryMatrix,
         fromMemoryMatrix,
         flips,
+        commandHeap,
+        command,
+        firstChild,
+        lastChild,
       };
     });
   };
 
   // eslint-disable-next-line arrow-body-style
   private sliderShift = (newValue: number) => {
-    const { memoryHeap } = this.state;
+    const { commandHeap } = this.state;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, arrow-body-style
     this.setState((state: State) => {
       return {
         value: newValue,
-        heap: memoryHeap[newValue],
+        heap: commandHeap[newValue].heap,
+        fromSpace: commandHeap[newValue].from,
+        toSpace: commandHeap[newValue].to,
+        command: commandHeap[newValue].type,
+        firstChild: commandHeap[newValue].left,
+        lastChild: commandHeap[newValue].right,
       };
     });
   };
 
   // eslint-disable-next-line arrow-body-style
-  private changeContent = (index: number) => {
+  private updateChild = (index: number) => {
     // eslint-disable-next-line react/destructuring-assignment
-    const newContent = this.state.heap[index];
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars, arrow-body-style
-    this.setState((state: State) => {
-      return {
-        content: newContent,
-      };
-    });
+    const value = this.state.heap ? this.state.heap[index] : 0;
+
+    if (this.isTag(value)) {
+      const FIRST_CHILD_SLOT = 2;
+      const LAST_CHILD_SLOT = 3;
+      // eslint-disable-next-line react/destructuring-assignment
+      const firstChild = this.state.heap[index + FIRST_CHILD_SLOT];
+      // eslint-disable-next-line react/destructuring-assignment
+      const lastChild = this.state.heap[index + LAST_CHILD_SLOT];
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars, arrow-body-style
+      this.setState((state: State) => {
+        return {
+          firstChild,
+          lastChild,
+        };
+      });
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars, arrow-body-style
+      this.setState((state: State) => {
+        return {
+          firstChild: -1,
+          lastChild: -1,
+        };
+      });
+    }
   };
 
   private getlengthFunction = () => {
     const { debuggerContext } = this.props;
-    const memoryHeap = debuggerContext.result.value
-      ? debuggerContext.result.value.get_memory_heap()
+    const commandHeap = debuggerContext.result.value
+      ? debuggerContext.result.value.get_command()
       : [];
-    return memoryHeap.length;
+    return commandHeap.length;
   };
 
   private isTag = (tag) => {
@@ -127,16 +169,22 @@ class CopyGC extends React.Component<Props, State> {
     return tags ? tags.includes(tag) : false;
   };
 
-  private getMemoryColor = (rowNumber, columnNumber) => {
-    const { column } = this.state;
-    const index = rowNumber * column + columnNumber;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { row } = this.state;
+  private getMemoryColor = (rowNumber, columnNumber, indexValue) => {
+    const { firstChild } = this.state;
+    const { lastChild } = this.state;
+    const { heap } = this.state;
+    const size1 = heap[firstChild + SIZE_SLOT];
+    const size2 = heap[lastChild + SIZE_SLOT];
+    // const { fromSpace, toSpace } = this.state;
     // eslint-disable-next-line react/destructuring-assignment
-    const value = this.state.heap ? this.state.heap[index] : 0;
+    const value = heap ? heap[indexValue] : 0;
     let color = '';
 
-    if (!value) {
+    if (indexValue >= firstChild && indexValue <= firstChild + size1) {
+      color = 'red';
+    } else if (indexValue >= lastChild && indexValue <= lastChild + size2) {
+      color = 'yellow';
+    } else if (!value) {
       color = '#707070';
     } else if (this.isTag(value)) {
       // is a tag
@@ -158,12 +206,11 @@ class CopyGC extends React.Component<Props, State> {
         {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
         <div>
           <p>This is for explanation</p>
+          <h3>{state.command}</h3>
           <p>
             Memory size: {state.memorySize} | To Space: {state.toSpace} | From
             Space: {state.fromSpace}
           </p>
-          <p>The content: {state.content}</p>
-          <p>The heap: {state.memoryHeap.lastIndexOf(state.heap)}</p>
           <p>{lengthOfFunction}</p>
           <div style={{ padding: 5 }}>
             <Slider
@@ -171,14 +218,14 @@ class CopyGC extends React.Component<Props, State> {
               min={0}
               max={lengthOfFunction > 0 ? lengthOfFunction - 1 : 0}
               onChange={this.sliderShift}
-              value={state.value <= 30 ? state.value : 0}
-              // labelValues={state.flips}
+              value={state.value <= lengthOfFunction ? state.value : 0}
+              labelValues={state.flips}
             />
           </div>
         </div>
         <div>
           <div>
-            <h3>To Space</h3>
+            <h3>{state.toSpace === 0 ? 'To Space' : 'From Space'}</h3>
             <div>
               {toMemoryMatrix && toMemoryMatrix.length > 0
                 ? toMemoryMatrix.map((item, row) => (
@@ -192,11 +239,15 @@ class CopyGC extends React.Component<Props, State> {
                       {item && item.length > 0
                         ? // eslint-disable-next-line @typescript-eslint/no-unused-vars, arrow-body-style
                           item.map((content, column) => {
-                            const color = this.getMemoryColor(row, column);
+                            const color = this.getMemoryColor(
+                              row + state.row / 2,
+                              column,
+                              content
+                            );
                             return (
                               // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
                               <div
-                                style={{ width: 14, height: 15 }}
+                                style={{ width: 14 }}
                                 // onMouseOver={() => this.changeContent(content)}
                               >
                                 <canvas
@@ -206,7 +257,14 @@ class CopyGC extends React.Component<Props, State> {
                                     backgroundColor: color,
                                     // margin: 2,
                                   }}
+                                  // onClick={() => this.updateChild(content)}
                                 />
+                                <span>
+                                  {state.firstChild === content ? '^' : ''}
+                                </span>
+                                <span>
+                                  {state.lastChild === content ? '*' : ''}
+                                </span>
                               </div>
                             );
                           })
@@ -217,7 +275,7 @@ class CopyGC extends React.Component<Props, State> {
             </div>
           </div>
           <div>
-            <h3>From Space</h3>
+            <h3>{state.fromSpace === 0 ? 'To Space' : 'From Space'}</h3>
             <div>
               {fromMemoryMatrix && fromMemoryMatrix.length > 0
                 ? fromMemoryMatrix.map((item, row) => (
@@ -231,7 +289,11 @@ class CopyGC extends React.Component<Props, State> {
                       {item && item.length > 0
                         ? // eslint-disable-next-line @typescript-eslint/no-unused-vars, arrow-body-style
                           item.map((content, column) => {
-                            const color = this.getMemoryColor(row, column);
+                            const color = this.getMemoryColor(
+                              row,
+                              column,
+                              content
+                            );
                             return (
                               // eslint-disable-next-line jsx-a11y/mouse-events-have-key-events
                               <div
@@ -246,6 +308,12 @@ class CopyGC extends React.Component<Props, State> {
                                     // margin: 2,
                                   }}
                                 />
+                                <span>
+                                  {state.firstChild === content ? '^' : ''}
+                                </span>
+                                <span>
+                                  {state.lastChild === content ? '*' : ''}
+                                </span>
                               </div>
                             );
                           })
