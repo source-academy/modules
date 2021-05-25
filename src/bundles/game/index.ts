4fddfdde14d8121c90120442b6ada71918cb5830
Gokul Rajiv <34594184+gok99@users.noreply.github.com>
2021-05-25 17:43:31 +0800

    [Game] Add Game module (#62)
    * Initial Game Port
    * Add Phaser and fix typings
    * Update documentation and types
    * Make dummy funcs for Typedoc
    * Wrap GameParams in __Params

---
+++ src/bundles/game/index.ts
@@ -0,0 +1,4 @@
+import { __Params } from './types';
+import gameFuncs from './functions';
+
+export default (_params: __Params) => gameFuncs(_params);
---
+++ src/bundles/game/types.ts
@@ -0,0 +1,40 @@
+import * as Phaser from 'phaser';
+
+export type ObjectConfig = { [attr: string]: any };
+
+export type RawGameElement =
+  | Phaser.GameObjects.Sprite
+  | Phaser.GameObjects.Text;
+
+export type RawGameShape =
+  | Phaser.GameObjects.Rectangle
+  | Phaser.GameObjects.Ellipse;
+
+export type RawGameObject = RawGameElement | RawGameShape;
+
+export type RawContainer = Phaser.GameObjects.Container;
+
+export type RawInputObject =
+  | Phaser.Input.InputPlugin
+  | Phaser.Input.Keyboard.Key;
+
+export type GameObject = {
+  type: string;
+  object: RawGameObject | RawInputObject | RawContainer | undefined;
+};
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export type __Params = {
+  game: GameParams;
+};
+
+export type GameParams = {
+  scene: Phaser.Scene;
+  phaser: any;
+  preloadImageMap: Map<string, string>;
+  preloadSoundMap: Map<string, string>;
+  preloadSpritesheetMap: Map<string, [string, object]>;
+  remotePath: string;
+  screenSize: { x: number; y: number };
+  createAward: (x: number, y: number, key: string) => Phaser.GameObjects.Sprite;
+};
