86264b20c781931a6be682c6e05be81eeb2a9264
Martin Henz <henz@comp.nus.edu.sg>
2021-06-29 14:38:18 +0800

    more finetuning

--- src/bundles/curve/functions.ts
+++ src/bundles/curve/functions.ts
@@ -1,26 +1,26 @@
 /**
- * The curves library provides functions for drawing "curves", i.e a sequence of
- * points that may/may not be connected by lines.
+ * The module `curve` provides functions for drawing *curves*, i.e. collections of
+ * *points*.
  *
- * A point is defined by its coordinates (x, y and z), and the color assigned to
+ * A *point* is defined by its coordinates (x, y and z), and the color assigned to
  * it (r, g, and b). A few constructors for points is given, for example
  * `make_color_point`. Selectors allow access to the coordinates and color
  * components, for example `x_of`.
  *
- * The central element of the library is the definition of Curve, a
- * unary function which takes a Number argument within the unit interval `[0,1]`
- * and returns a point. If `C` is a Curve, then the starting point of the curve
+ * A *curve* is a 
+ * unary function which takes a number argument within the unit interval `[0,1]`
+ * and returns a point. If `C` is a curve, then the starting point of the curve
  * is always `C(0)`, and the ending point is always `C(1)`.
  *
- * A Curve transformation is a function that takes a Curve as argument and
- * returns a Curve. Examples of Curve transformations are `scale` and `translate`.
+ * A *curve transformation* is a function that takes a curve as argument and
+ * returns a curve. Examples of curve transformations are `scale` and `translate`.
  *
- * Finally, a Curve drawer is function that takes a Number argument and returns
- * a function that takes in a Curve and visualises it in the output screen is
+ * A *curve drawer* is function that takes a number argument and returns
+ * a function that takes a curve as argument and visualises it in the output screen is
  * shown in the Source Academy in the tab with the "Curves Canvas" icon (image).
- * Examples of drawer functions are `draw_points_on` and `draw_connected`.
- *
- * [Example:](https://share.sourceacademy.org/unitcircle)
+ * The following [example](https://share.sourceacademy.org/unitcircle) uses
+ * the curve drawer `draw_connected_full_view` to display a curve called
+ * `unit_circle`.
  * ```
  * import { make_point, draw_connected_full_view } from "curve";
  * function unit_circle(t) {
