a1cd7a5aebfe972f344ff151281ce18f1d9d0be3
Martin Henz <henz@comp.nus.edu.sg>
2021-07-08 16:03:13 +0800

    Scrabble (#77)
    * functions uniformly extended to points
    
    * first version

---
+++ src/bundles/scrabble_words/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable max-classes-per-file */
+import { scrabble_words_array, scrabble_words_list } from './words';
+
+/**
+ * Scrabble words for Source Academy
+ * @author Martin Henz
+ */
+
+export default () => ({
+  scrabble_words_array,
+  scrabble_words_list,
+});
