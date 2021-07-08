17d237c8921cb339512a66114253fefd24e066fd
Martin Henz <henz@comp.nus.edu.sg>
2021-07-08 15:58:58 +0800

    first version

---
+++ src/bundles/scrabble_words/__tests__/index.ts
@@ -0,0 +1,15 @@
+import { scrabble_words_array, scrabble_words_list } from '../words';
+
+function list_ref(list, n) {
+    return n === 0 ? list[0] : list_ref(list[1], n - 1);
+}
+
+// Test functions
+
+test('get the first word in the array', () => {
+  expect(scrabble_words_array[0]).toBe("aa");
+});
+
+test('get the first word in the list', () => {
+  expect(list_ref(scrabble_words_list, 0)).toBe("aa");
+});
