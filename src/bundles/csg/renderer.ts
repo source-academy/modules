37964c1aba9ba48309edd1aab2277a294319be11
Cloud <44526554+Cloud7050@users.noreply.github.com>
2022-02-25 13:22:14 +0800

    ref(csg renderer): Tidy renderer structure prior to reintroducing more parts

--- src/bundles/csg/renderer.ts
+++ src/bundles/csg/renderer.ts
@@ -17,59 +17,82 @@ import {
   Shape,
 } from './utilities';
 
-/* [Exports] */
-export default function render(canvas: HTMLCanvasElement, shape: Shape) {
+/* [Main] */
+function makeWrappedRenderer(
+  canvas: HTMLCanvasElement
+): WrappedRenderer.Function {
   const prepareRenderOptions: PrepareRender.AllOptions = {
     glOptions: {
-      gl: canvas.getContext('webgl2') ?? undefined,
+      canvas,
     },
   };
-  const wrappedRenderer: WrappedRenderer.Function = prepareRender(
-    prepareRenderOptions
-  );
+  return prepareRender(prepareRenderOptions);
+}
 
-  const perspectiveCameraState: PerspectiveCameraState = {
-    ...perspectiveCameraStateDefaults,
-    position: [1.414811372756958, 1.7292139530181885, 2.200817823410034],
-  };
-
+function adjustCameraAngle(
+  perspectiveCameraState: PerspectiveCameraState
+): void {
   // Modify the position & view of the passed camera state,
   // based on its existing position of the viewer (eye),
   // target point the viewer is looking at (centre) & up axis
   perspectiveCamera.update(perspectiveCameraState);
+}
 
+function makeEntities(shape): Entity[] {
   const entities: Entity[] = entitiesFromSolids(undefined, shape.getSolid());
+
   if (shape.addAxis) entities.push(new AxisEntity.Class());
   if (shape.addMultiGrid) entities.push(new MultiGridEntity.Class());
 
+  return entities;
+}
+
+function doDynamicResize(
+  canvas: HTMLCanvasElement,
+  perspectiveCameraState: PerspectiveCameraState
+): void {
+  const canvasBounds: DOMRect = canvas.getBoundingClientRect();
+  const devicePixelRatio: number = window.devicePixelRatio;
+
+  // Account for display scaling
+  const width: number = canvasBounds.width * devicePixelRatio;
+  const height: number = canvasBounds.height * devicePixelRatio;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  // Modify the projection, aspect ratio & viewport
+  perspectiveCamera.setProjection(
+    perspectiveCameraState,
+    perspectiveCameraState,
+    new CameraViewportDimensions(width, height)
+  );
+}
+
+/* [Exports] */
+export default function render(canvas: HTMLCanvasElement, shape: Shape): void {
+  const wrappedRenderer: WrappedRenderer.Function = makeWrappedRenderer(canvas);
+
+  // Create our own state to modify based on the defaults
+  const perspectiveCameraState: PerspectiveCameraState = {
+    ...perspectiveCameraStateDefaults,
+    position: [1.414811372756958, 1.7292139530181885, 2.200817823410034],
+  };
+  adjustCameraAngle(perspectiveCameraState);
+
+  // Data to pass to the wrapped renderer we made, below
   const wrappedRendererData: WrappedRenderer.AllData = {
-    entities,
+    entities: makeEntities(shape),
     drawCommands: prepareDrawCommands,
     camera: perspectiveCameraState,
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // Create a callback function.
+  // Request animation frame with it once; it will loop itself from there
   function animationCallback(_timestamp: DOMHighResTimeStamp) {
-    // [Support dynamic resizing]
-    const canvasBounds: DOMRect = canvas.getBoundingClientRect();
-    const devicePixelRatio: number = window.devicePixelRatio;
+    doDynamicResize(canvas, perspectiveCameraState);
 
-    // Account for display scaling
-    const width: number = canvasBounds.width * devicePixelRatio;
-    const height: number = canvasBounds.height * devicePixelRatio;
-
-    // eslint-disable-next-line no-param-reassign
-    canvas.width = width;
-    // eslint-disable-next-line no-param-reassign
-    canvas.height = height;
-
-    // Modify the projection, aspect ratio & viewport
-    perspectiveCamera.setProjection(
-      perspectiveCameraState,
-      perspectiveCameraState,
-      new CameraViewportDimensions(width, height)
-    );
-
+    // Trigger render once processing for the current frame is done
     wrappedRenderer(wrappedRendererData);
 
     window.requestAnimationFrame(animationCallback);
