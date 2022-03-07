ea29e7b8c6e7b16fc11f35e271794227ca6136e7
leeyi45 <leeyi45@gmail.com>
2022-03-08 03:13:31 +0800

    Moved StereoSound to MultItemDisplay

--- src/bundles/stereo_sound/types.ts
+++ src/bundles/stereo_sound/types.ts
@@ -1,3 +1,5 @@
+import { ModuleState } from 'js-slang';
+
 /* eslint-disable no-unused-vars */
 export type Pair<H, T> = [H, T];
 export type EmptyList = null;
@@ -7,12 +9,14 @@ export type Wave = (...t: any) => number;
 export type Sound = Pair<Pair<Wave, Wave>, number>;
 export type SoundProducer = (...t: any) => Sound;
 export type SoundTransformer = (s: Sound) => Sound;
-export type AudioElement = HTMLAudioElement;
 export type ErrorLogger = (
   error: string | string[],
   isSlangError?: boolean
 ) => void;
 export type AudioPlayed = {
   toReplString: () => string;
-  init: (audio: HTMLAudioElement) => boolean;
+  dataUri: string;
 };
+export class StereoSoundsModuleState implements ModuleState {
+  constructor(public audioPlayed: AudioPlayed[]) {}
+}
