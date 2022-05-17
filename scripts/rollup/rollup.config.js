0793dfb5702348871b27c250cccd18f8bbfa70da
Cloud <44526554+Cloud7050@users.noreply.github.com>
2022-05-18 05:49:22 +0800

    feat(all): Scrap skipping bundles/tabs, rather than implement separate timestamp tracking & logic. Still achieves goal of greatly speeding up rebuilds

--- scripts/rollup/rollup.config.js
+++ scripts/rollup/rollup.config.js
@@ -14,18 +14,11 @@ export default function (commandLineArguments) {
   );
   let { bundleNames, tabNames } = rollupBundleNames;
 
-  //NOTE When using this rollup config, optionally specify extra command line
-  // arguments, eg in npm scripts
-  let buildBundles = !commandLineArguments.skipBundles;
-  let buildTabs = !commandLineArguments.skipTabs;
-
   // Delete so rollup ignores the custom argument and doesn't log a warning
   delete commandLineArguments.quick;
-  delete commandLineArguments.skipBundles;
-  delete commandLineArguments.skipTabs;
 
-  let bundleConfigs = buildBundles ? bundleNamesToConfigs(bundleNames) : [];
-  let tabConfigs = buildTabs ? tabNamesToConfigs(tabNames) : [];
+  let bundleConfigs = bundleNamesToConfigs(bundleNames);
+  let tabConfigs = tabNamesToConfigs(tabNames);
 
   // Rollup bundle configs, for module bundles and/or module tabs
   let rollupBundleConfigs = [...bundleConfigs, ...tabConfigs];
