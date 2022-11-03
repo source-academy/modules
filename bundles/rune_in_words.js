3d93bba450d4ebe76aacd541ce90160481465599
martin-henz <martin-henz@users.noreply.github.com>
2022-11-03 13:43:52 +0000

    deploy: 09a47df94c590a835e26fefc9f5d5fb9d9190bad

---
+++ bundles/rune_in_words.js
@@ -0,0 +1,266 @@
+(function () {
+  'use strict';
+  var exports = {};
+  (function () {
+    const env = {};
+    try {
+      if (process) {
+        process.env = Object.assign({}, process.env);
+        Object.assign(process.env, env);
+        return;
+      }
+    } catch (e) {}
+    globalThis.process = {
+      env: env
+    };
+  })();
+  function throwIfNotRune(name) {
+    var runes = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+      runes[_i - 1] = arguments[_i];
+    }
+    runes.forEach(function (rune) {
+      if (!(typeof rune === "string")) {
+        throw Error(("").concat(name, " expects a rune as argument."));
+      }
+    });
+  }
+  var getSquare = function getSquare() {
+    return "square";
+  };
+  var getBlank = function getBlank() {
+    return "blank";
+  };
+  var getRcross = function getRcross() {
+    return "rcross";
+  };
+  var getSail = function getSail() {
+    return "sail";
+  };
+  var getTriangle = function getTriangle() {
+    return "triangle";
+  };
+  var getCorner = function getCorner() {
+    return "corner";
+  };
+  var getNova = function getNova() {
+    return "nova";
+  };
+  var getCircle = function getCircle() {
+    return "circle";
+  };
+  var getHeart = function getHeart() {
+    return "heart";
+  };
+  var getPentagram = function getPentagram() {
+    return "pentagram";
+  };
+  var getRibbon = function getRibbon() {
+    return "ribbon";
+  };
+  var square = getSquare();
+  var blank = getBlank();
+  var rcross = getRcross();
+  var sail = getSail();
+  var triangle = getTriangle();
+  var corner = getCorner();
+  var nova = getNova();
+  var circle = getCircle();
+  var heart = getHeart();
+  var pentagram = getPentagram();
+  var ribbon = getRibbon();
+  function from_url(imageUrl) {
+    return "url(" + imageUrl + ")";
+  }
+  function scale_independent(ratio_x, ratio_y, rune) {
+    throwIfNotRune("scale_independent", rune);
+    return "scaled(" + rune + ", " + ratio_x + ", " + ratio_y + ")";
+  }
+  function scale(ratio, rune) {
+    throwIfNotRune("scale", rune);
+    return scale_independent(ratio, ratio, rune);
+  }
+  function translate(x, y, rune) {
+    throwIfNotRune("translate", rune);
+    return "translated(" + rune + ", " + x + ", " + y + ")";
+  }
+  function rotate(rad, rune) {
+    throwIfNotRune("rotate", rune);
+    return "rotated(" + rune + ", " + rad + ")";
+  }
+  function stack_frac(frac, rune1, rune2) {
+    throwIfNotRune("stack_frac", rune1);
+    throwIfNotRune("stack_frac", rune2);
+    return "stack_frac(" + frac + ", " + rune1 + ", " + rune2 + ")";
+  }
+  function stack(rune1, rune2) {
+    throwIfNotRune("stack", rune1, rune2);
+    return "stack(" + rune1 + ", " + rune2 + ")";
+  }
+  function stackn(n, rune) {
+    throwIfNotRune("stackn", rune);
+    return "stackn(" + n + ", " + rune + ")";
+  }
+  function quarter_turn_right(rune) {
+    throwIfNotRune("quarter_turn_right", rune);
+    return "quarter_turn_right(" + rune + ")";
+  }
+  function quarter_turn_left(rune) {
+    throwIfNotRune("quarter_turn_left", rune);
+    return "quarter_turn_left(" + rune + ")";
+  }
+  function turn_upside_down(rune) {
+    throwIfNotRune("turn_upside_down", rune);
+    return "quarter_upside_down(" + rune + ")";
+  }
+  function beside_frac(frac, rune1, rune2) {
+    throwIfNotRune("beside_frac", rune1, rune2);
+    return "beside_frac(" + frac + ", " + rune1 + ", " + rune2 + ")";
+  }
+  function beside(rune1, rune2) {
+    throwIfNotRune("beside", rune1, rune2);
+    return "stack(" + rune1 + ", " + rune2 + ")";
+  }
+  function flip_vert(rune) {
+    throwIfNotRune("flip_vert", rune);
+    return "flip_vert(" + rune + ")";
+  }
+  function flip_horiz(rune) {
+    throwIfNotRune("flip_horiz", rune);
+    return "flip_horiz(" + rune + ")";
+  }
+  function make_cross(rune) {
+    throwIfNotRune("make_cross", rune);
+    return stack(beside(quarter_turn_right(rune), turn_upside_down(rune)), beside(rune, quarter_turn_left(rune)));
+  }
+  function repeat_pattern(n, pattern, initial) {
+    if (n === 0) {
+      return initial;
+    }
+    return pattern(repeat_pattern(n - 1, pattern, initial));
+  }
+  function overlay_frac(frac, rune1, rune2) {
+    throwIfNotRune("overlay_frac", rune1);
+    throwIfNotRune("overlay_frac", rune2);
+    return "overlay_frac(" + frac + ", " + rune1 + ", " + rune2 + ")";
+  }
+  function overlay(rune1, rune2) {
+    throwIfNotRune("overlay", rune1);
+    throwIfNotRune("overlay", rune2);
+    return "overlay(" + rune1 + ", " + rune2 + ")";
+  }
+  function color(rune, r, g, b) {
+    throwIfNotRune("color", rune);
+    return "color(" + rune + ", " + r + ", " + g + ", " + b + ")";
+  }
+  function random_color(rune) {
+    throwIfNotRune("random_color", rune);
+    return "random(" + rune + ")";
+  }
+  function red(rune) {
+    throwIfNotRune("red", rune);
+    return "red(" + rune + ")";
+  }
+  function pink(rune) {
+    throwIfNotRune("pink", rune);
+    return "pink(" + rune + ")";
+  }
+  function purple(rune) {
+    throwIfNotRune("purple", rune);
+    return "purple(" + rune + ")";
+  }
+  function indigo(rune) {
+    throwIfNotRune("indigo", rune);
+    return "indigo(" + rune + ")";
+  }
+  function blue(rune) {
+    throwIfNotRune("blue", rune);
+    return "blue(" + rune + ")";
+  }
+  function green(rune) {
+    throwIfNotRune("green", rune);
+    return "green(" + rune + ")";
+  }
+  function yellow(rune) {
+    throwIfNotRune("yellow", rune);
+    return "yellow(" + rune + ")";
+  }
+  function orange(rune) {
+    throwIfNotRune("orange", rune);
+    return "orange(" + rune + ")";
+  }
+  function brown(rune) {
+    throwIfNotRune("brown", rune);
+    return "brown(" + rune + ")";
+  }
+  function black(rune) {
+    throwIfNotRune("black", rune);
+    return "black(" + rune + ")";
+  }
+  function white(rune) {
+    throwIfNotRune("white", rune);
+    return "white(" + rune + ")";
+  }
+  function show(rune) {
+    throwIfNotRune("show", rune);
+    return rune;
+  }
+  function anaglyph(rune) {
+    throwIfNotRune("anaglyph", rune);
+    return rune;
+  }
+  function hollusion(rune) {
+    throwIfNotRune("hollusion", rune);
+    return rune;
+  }
+  exports.anaglyph = anaglyph;
+  exports.beside = beside;
+  exports.beside_frac = beside_frac;
+  exports.black = black;
+  exports.blank = blank;
+  exports.blue = blue;
+  exports.brown = brown;
+  exports.circle = circle;
+  exports.color = color;
+  exports.corner = corner;
+  exports.flip_horiz = flip_horiz;
+  exports.flip_vert = flip_vert;
+  exports.from_url = from_url;
+  exports.green = green;
+  exports.heart = heart;
+  exports.hollusion = hollusion;
+  exports.indigo = indigo;
+  exports.make_cross = make_cross;
+  exports.nova = nova;
+  exports.orange = orange;
+  exports.overlay = overlay;
+  exports.overlay_frac = overlay_frac;
+  exports.pentagram = pentagram;
+  exports.pink = pink;
+  exports.purple = purple;
+  exports.quarter_turn_left = quarter_turn_left;
+  exports.quarter_turn_right = quarter_turn_right;
+  exports.random_color = random_color;
+  exports.rcross = rcross;
+  exports.red = red;
+  exports.repeat_pattern = repeat_pattern;
+  exports.ribbon = ribbon;
+  exports.rotate = rotate;
+  exports.sail = sail;
+  exports.scale = scale;
+  exports.scale_independent = scale_independent;
+  exports.show = show;
+  exports.square = square;
+  exports.stack = stack;
+  exports.stack_frac = stack_frac;
+  exports.stackn = stackn;
+  exports.translate = translate;
+  exports.triangle = triangle;
+  exports.turn_upside_down = turn_upside_down;
+  exports.white = white;
+  exports.yellow = yellow;
+  Object.defineProperty(exports, '__esModule', {
+    value: true
+  });
+  return exports;
+})
