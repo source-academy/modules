d0bc643571119b4750a6391d36c75aa0b0da6a1f
larrywang0701 <113608053+larrywang0701@users.noreply.github.com>
2023-02-13 22:55:27 +0800

    Add files via upload

---
+++ src/bundles/repl/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Bundle for Source Academy Programmable REPL module
+ * @module repl
+ * @author Wang Zihan
+ */
+
+export {
+  invoke_repl,
+  module_display,
+  rich_display,
+  set_editor_background_image,
+  set_editor_font_size,
+  //default_js_slang, // Commented because this is currently not supported in production version. Need changes in js-slang side to make it work.
+} from './functions';
