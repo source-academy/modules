125aa73a4939a06e6dd3ea4a86c96d118df489f6
larrywang0701 <113608053+larrywang0701@users.noreply.github.com>
2023-02-24 20:13:53 +0800

    Full default_js_slang function support to with js-slang pr

--- src/bundles/repl/functions.ts
+++ src/bundles/repl/functions.ts
@@ -16,10 +16,10 @@ context.moduleContexts.repl.state = INSTANCE;
  *
  * @category Main
  */
-export function invoke_repl(evalFunc: Function) {
+export function set_evaluator(evalFunc: Function) {
   if (!(evalFunc instanceof Function)) {
     const typeName = typeof (evalFunc);
-    throw new Error(`Wrong parameter type "${typeName}' in function "invoke_repl". It supposed to be a function and it's the entrance function of your metacircular evaulator.`);
+    throw new Error(`Wrong parameter type "${typeName}' in function "set_evaluator". It supposed to be a function and it's the entrance function of your metacircular evaulator.`);
   }
   INSTANCE.evalFunction = evalFunc;
   return {
@@ -70,17 +70,17 @@ export function set_font_size(font_size_px : number) {
 
 
 /**
- * When use this function as the entrance function in the parameter of "invoke_repl", the Programmable Repl will directly use the default js-slang interpreter to run your program in Programmable Repl editor. Do not directly call this function in your own code.
+ * When use this function as the entrance function in the parameter of "set_evaluator", the Programmable Repl will directly use the default js-slang interpreter to run your program in Programmable Repl editor. Do not directly call this function in your own code.
  * @param {program} Do not directly set this parameter in your code.
  * @param {safeKey} A parameter that is designed to prevent student from directly calling this function in Source language.
  *
  * @category Main
  */
 export function default_js_slang(program : string, safeKey : any) : any {
-  // When the function is normally called by invoke_repl function, safeKey is set to "document.body", which has a type "Element".
+  // When the function is normally called by set_evaluator function, safeKey is set to "document.body", which has a type "Element".
   // Students can not create objects and use HTML Elements in Source due to limitations and rules in Source, so they can't set the safeKey to a HTML Element, thus they can't use this function in Source.
   if (!(safeKey instanceof Element)) {
-    throw new Error('Invaild Call: Function "default_js_slang" can not be directly called by user\'s code in editor. You should use it as the parameter of the function "invoke_repl"');
+    throw new Error('Invaild Call: Function "default_js_slang" can not be directly called by user\'s code in editor. You should use it as the parameter of the function "set_evaluator"');
   }
   return INSTANCE.runInJsSlang(program);
 }
