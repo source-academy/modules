33c9f83e60590803b1e71c0e8cc79eafa55adeae
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-03-17 22:06:36 +0800

    Added ShapeGameObjects - Added rectange, circle, triangle shapes - Renamed functions to snake_case for consistency - Refactored GameObject class code - Smoother key down and up detection - Pointer over object detection - Debug mode to show shape/hitbox - Right Click detection - Play,Stoping,Looping, creation of audio clips - Setting alpha of gameobject

--- src/bundles/arcade_two_d/constants.ts
+++ src/bundles/arcade_two_d/constants.ts
@@ -8,12 +8,12 @@ export const DEFAULT_FPS: number = 30;
 export const DEFAULT_VOLUME: number = 0.5;
 
 // Maximum allowed values for canvas
-export const MAX_HEIGHT: number = 800;
-export const MIN_HEIGHT: number = 400;
-export const MAX_WIDTH: number = 800;
-export const MIN_WIDTH: number = 400;
-export const MAX_SCALE: number = 2;
-export const MIN_SCALE: number = 0.5;
+export const MAX_HEIGHT: number = 1000;
+export const MIN_HEIGHT: number = 100;
+export const MAX_WIDTH: number = 1000;
+export const MIN_WIDTH: number = 100;
+export const MAX_SCALE: number = 10;
+export const MIN_SCALE: number = 0.1;
 export const MAX_FPS: number = 120;
 export const MIN_FPS: number = 1;
 export const MAX_VOLUME: number = 1;
@@ -21,4 +21,4 @@ export const MIN_VOLUME: number = 0;
 
 // A mode where the hitboxes is shown in the canvas for debugging purposes,
 // and possibly other debug information
-export const DEBUG_MODE: boolean = false;
+export const DEFAULT_DEBUG: boolean = false;
--- src/bundles/arcade_two_d/index.ts
+++ src/bundles/arcade_two_d/index.ts
@@ -1,7 +1,7 @@
 /**
  * Bundle for Source Academy Arcade2D module.
  *
- * Arcade2D allows users to create their own games.
+ * Arcade2D allows users to create their own 2D games with Source §3 and above variants.
  *
  * @module arcade_two_d
  * @author Titus Chew Xuan Jun
@@ -9,29 +9,41 @@
  */
 
 export {
-  createShapeGameObject,
-  createTextGameObject,
-  createSpriteGameObject,
-  queryGameObjectId,
-  queryGameObjectPosition,
-  queryGameObjectColor,
-  queryGameObjectRotation,
-  queryGameObjectScale,
-  queryGameObjectText,
-  queryPointerPosition,
-  updateGameObjectPosition,
-  updateGameObjectRotation,
-  updateGameObjectScale,
-  updateGameObjectColor,
-  updateGameObjectText,
+  create_circle,
+  create_rectangle,
+  create_triangle,
+  create_sprite,
+  create_text,
+  query_color,
+  query_flip,
+  query_id,
+  query_pointer_position,
+  query_position,
+  query_rotation,
+  query_scale,
+  query_text,
+  update_color,
+  update_flip,
+  update_position,
+  update_rotation,
+  update_scale,
+  update_text,
+  update_to_top,
   set_fps,
   set_dimensions,
   set_scale,
   set_volume,
   get_game_time,
+  get_loop_count,
+  enable_debug,
   input_key_down,
   input_left_mouse_down,
+  input_right_mouse_down,
+  pointer_over_gameobject,
   update_loop,
   build_game,
-  display_hello_world,
+  create_audio,
+  loop_audio,
+  stop_audio,
+  play_audio,
 } from './functions';
