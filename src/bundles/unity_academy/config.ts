5cf0a195922906f26808091819018abaa3bfe21e
larrywang0701 <113608053+larrywang0701@users.noreply.github.com>
2023-03-30 17:07:54 +0800

    Unity Academy files

--- src/bundles/unity_academy/config.ts
+++ src/bundles/unity_academy/config.ts
@@ -1,2 +1,1 @@
-// export const UNITY_ACADEMY_BACKEND_URL = 'https://unity-academy.s3.ap-southeast-1.amazonaws.com/';
-export const UNITY_ACADEMY_BACKEND_URL = 'http://localhost:2000/';
+export const UNITY_ACADEMY_BACKEND_URL = 'https://unity-academy.s3.ap-southeast-1.amazonaws.com/';
