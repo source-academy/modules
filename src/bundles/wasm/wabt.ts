980ba228cacfa0bd109db83cf95ac06673a76d4b
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-04-09 12:18:12 +0800

    Add comments for module methods

--- src/bundles/wasm/wabt.ts
+++ src/bundles/wasm/wabt.ts
@@ -1,17 +1,17 @@
-import { getParseTree, compileParseTree, compile } from 'source-academy-wabt';
-import {objectToLinkedList} from 'source-academy-utils';
+import { compile } from 'source-academy-wabt';
+import { objectToLinkedList } from 'source-academy-utils';
 
 /**
- * Compile stuff
- * @param program 
- * @returns 
+ * Compile a (hopefully valid) WebAssembly Text module to binary.
+ * @param program program to compile
+ * @returns an array of 8-bit unsigned integers.
  */
 export const wcompile = (program: string) => Array.from(compile(program));
 
 /**
- * Run stuff
- * @param buffer 
- * @returns 
+ * Run a compiled WebAssembly Binary Buffer.
+ * @param buffer an array of 8-bit unsigned integers to run
+ * @returns a linked list of exports that the relevant WebAssembly Module exports
  */
 export const wrun = (buffer: number[] | Uint8Array) => {
   if (buffer instanceof Array) {
