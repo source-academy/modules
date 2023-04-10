f639457bb13e10b4016a22e590047125160dcf23
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-04-10 12:04:59 +0800

    updated snakeCaseRegex to reflect module naming

--- scripts/bin/templates/utilities.js
+++ scripts/bin/templates/utilities.js
@@ -1,4 +1,4 @@
-const snakeCaseRegex = /\b[a-z]+(?:_[a-z]+)*\b/u;
+const snakeCaseRegex = /\b[a-z0-9]+(?:_[a-z0-9]+)*\b/u;
 const pascalCaseRegex = /^[A-Z][a-z]+(?:[A-Z][a-z]+)*$/u;
 export function isSnakeCase(string) {
     return snakeCaseRegex.test(string);
