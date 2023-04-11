bf52ec6f797bd5506fc50ed028dbac7ba0e244d9
Titus Chew <39845485+tituschewxj@users.noreply.github.com>
2023-04-12 00:04:51 +0800

    normalize line endings

---
+++ src/bundles/arcade_2d/constants.ts
@@ -0,0 +1,24 @@
+// The default values. These values will likely change.
+
+// Default values of canvas
+export const DEFAULT_WIDTH: number = 600;
+export const DEFAULT_HEIGHT: number = 600;
+export const DEFAULT_SCALE: number = 1;
+export const DEFAULT_FPS: number = 30;
+export const DEFAULT_VOLUME: number = 0.5; // Unused
+
+// Maximum allowed values for canvas
+export const MAX_HEIGHT: number = 1000;
+export const MIN_HEIGHT: number = 100;
+export const MAX_WIDTH: number = 1000;
+export const MIN_WIDTH: number = 100;
+export const MAX_SCALE: number = 10;
+export const MIN_SCALE: number = 0.1;
+export const MAX_FPS: number = 120;
+export const MIN_FPS: number = 1;
+export const MAX_VOLUME: number = 1;
+export const MIN_VOLUME: number = 0;
+
+// A mode where the hitboxes is shown in the canvas for debugging purposes,
+// and debug log information
+export const DEFAULT_DEBUG: boolean = false;
