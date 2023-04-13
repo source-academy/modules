344abb9a4eb6c209d8f1e05c9bb09200cf5be45a
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-04-13 19:29:04 +0800

    Change line endings to windows

--- src/bundles/wasm/index.ts
+++ src/bundles/wasm/index.ts
@@ -5,7 +5,7 @@
  * Simple 'add' library:
  * ```wat
  * import {wcompile, wrun} from "wasm";
- * 
+ *
  * const program = `
  * (module
  *     (func (param f64) (param f64) (result f64)
@@ -15,24 +15,24 @@
  *     (export "add" (func 0))
  *   )
  * `;
- * 
- * 
+ *
+ *
  * const binary = wcompile(program);
  * const exports = wrun(binary);
- * 
+ *
  * display(binary);
  * display_list(exports);
- * 
+ *
  * const add_fn = head(tail(exports));
- * 
+ *
  * display(add_fn(10, 35));
  * ```
- * 
+ *
  * 'Calculator Language':
  * ```wat
  * // Type your program in here!
  * import {wcompile, wrun} from "wasm";
- * 
+ *
  * const program = `
  * (module
  *     (func (param f64) (param f64) (result f64)
@@ -56,13 +56,13 @@
  *     (export "mul" (func 2))
  *     (export "div" (func 3))
  * )`;
- * 
- * 
+ *
+ *
  * const encoding = wcompile(program);
  * let exports = wrun(encoding);
- * 
+ *
  * display_list(exports);
- * 
+ *
  * const div = head(tail(exports));
  * exports = tail(tail(exports));
  * const mul = head(tail(exports));
@@ -70,13 +70,13 @@
  * const sub = head(tail(exports));
  * exports = tail(tail(exports));
  * const add = head(tail(exports));
- * 
+ *
  * display(div(10, -35));
  * display(mul(10, 12347));
  * display(sub(12347, 12347));
  * display(add(10, 0));
  * ```
- * 
+ *
  *
  * @module wasm
  * @author Kim Yongbeom
