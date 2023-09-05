3ec40f5d593a87840d5c51c085e825317dd3fa1a
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-09-05 14:41:15 +0800

    devfeat(csg): Add primitives sample

---
+++ src/bundles/csg/samples/primitives.js
@@ -0,0 +1,37 @@
+// Source §4
+// Showcase of all 11 primitive Shapes provided by default
+
+let primitives = [
+    cube,
+    rounded_cube,
+    cylinder,
+    rounded_cylinder,
+    sphere,
+    geodesic_sphere,
+    pyramid,
+    cone,
+    prism,
+    star,
+    torus,
+];
+
+function translate_x(entity, factor) {
+    return translate(entity, factor, 0, 0);
+}
+
+function translate_y(entity, factor) {
+    return translate(entity, 0, factor, 0);
+}
+
+let l = build_list(
+    i => translate_y(
+        translate_x(
+            primitives[i](silver),
+            ((i % 4) - 2) * 2
+        ),
+        (math_floor(i / 4) - 2) * 2
+    ),
+    11
+);
+
+render_grid(group(l));
