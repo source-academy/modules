c0b7844d2435fee20e66f467bc961c6d0a57296d
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-09-06 10:24:52 +0800

    feat(csg): Update sierpinski sample

--- src/bundles/csg/samples/sierpinski.js
+++ src/bundles/csg/samples/sierpinski.js
@@ -1,28 +1,49 @@
 // Source §1
-// Prof Martin's Sierpinski triangle fractal
+// Prof Martin's Sierpinski fractals
 
-import { union, translate, scale, render, pyramid }
+import { union, translate, scale, render, pyramid, sphere, cube }
 from 'csg';
-
-const r = 1; // vertical stretch factor
-const shape = scale(pyramid('#edd4c8'), 1, 1, r);
 
 function repeat(n, trans, s) {
     return n === 0
-            ? s
-            : repeat(n - 1, trans, trans(s));
+           ? s
+           : repeat(n - 1, trans, trans(s));
 }
 
-function sierpinski(o) {
-    const t1 = translate(o,  0.5,  0.5, -r);
-    const t2 = translate(o, -0.5,  0.5, -r);
-    const t3 = translate(o,  0.5, -0.5, -r);
-    const t4 = translate(o, -0.5, -0.5, -r);
-    const s = union(o,
-                    union(union(t1, t2),
-                            union(t3, t4)));
-    const s_scaled = scale(s, 0.5, 0.5, 0.5);
-    return s_scaled;
+// sierpinski returns a shape transformer
+// following Sierpinski's 3D fractal scheme
+// v: vertical displacement of original shape
+//.   for lower level shapes
+// h: horizontal displacement of original shape
+//.   for lower level shapes
+function sierpinski(v, h) {
+    return o => {
+        const t1 = translate(o,  h,  h, -v);
+        const t2 = translate(o, -h,  h, -v);
+        const t3 = translate(o,  h, -h, -v);
+        const t4 = translate(o, -h, -h, -v);
+        const c = union(o,
+                        union(union(t1, t2),
+                              union(t3, t4)));
+        const c_scaled = scale(c, 0.5, 0.5, 0.5);
+        return c_scaled;
+    };
 }
 
-render(repeat(5, sierpinski, shape));
+render(repeat(5,
+              sierpinski(1, 0.5),
+              pyramid('#edd4c8')));
+
+// spheres are computationally expensive
+// only try repeat 2
+/*
+render(repeat(2,
+              sierpinski(0.75, 0.75),
+              sphere('#edd4c8')));
+*/
+
+/*
+render(repeat(6,
+              sierpinski(1, 1),
+              cube('#edd4c8')));
+*/
