9624a2619e5055e6a33490204e319fbb0efd2f86
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-09-06 08:17:16 +0800

    feat(csg): Update sierpinski sample for new primitive

--- src/bundles/csg/samples/sierpinski.js
+++ src/bundles/csg/samples/sierpinski.js
@@ -1,11 +1,10 @@
 // Source §1
 // Prof Martin's Sierpinski triangle fractal
 
-import { union, translate, scale,
-group, render, pyramid, sphere, cube }
+import { union, translate, scale, render, pyramid }
 from 'csg';
 
-const r = 0.75; // vertical stretch factor
+const r = 1; // vertical stretch factor
 const shape = scale(pyramid('#edd4c8'), 1, 1, r);
 
 function repeat(n, trans, s) {
@@ -15,10 +14,10 @@ function repeat(n, trans, s) {
 }
 
 function sierpinski(o) {
-    const t1 = translate(o,  0. , 0.5, -r);
-    const t2 = translate(o,  0.5, 0  , -r);
-    const t3 = translate(o, -0.5, 0  , -r);
-    const t4 = translate(o,  0. ,-0.5, -r);
+    const t1 = translate(o,  0.5,  0.5, -r);
+    const t2 = translate(o, -0.5,  0.5, -r);
+    const t3 = translate(o,  0.5, -0.5, -r);
+    const t4 = translate(o, -0.5, -0.5, -r);
     const s = union(o,
                     union(union(t1, t2),
                             union(t3, t4)));
