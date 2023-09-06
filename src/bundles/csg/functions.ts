f4e7f3bb54a161f61859f8f398c76998711437ef
Cloud <44526554+Cloud7050@users.noreply.github.com>
2023-09-06 10:07:19 +0800

    docs(csg): Link to GitHub samples in summary

--- src/bundles/csg/functions.ts
+++ src/bundles/csg/functions.ts
@@ -46,6 +46,8 @@
  * render_grid_axes(snowglobe);
  * ```
  *
+ * More samples can be found at: https://github.com/source-academy/modules/tree/master/src/bundles/csg/samples
+ *
  * @module csg
  * @author Joel Leow
  * @author Liu Muchen
