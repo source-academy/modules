5227c17dbb42b4c04de5aa9cc3a71864fab118b0
leeyi45 <leeyi45@gmail.com>
2023-09-06 18:14:22 +0800

    Update tsconfigs

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,10 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   displayName: 'Modules',
-  preset: 'ts-jest',
+  // preset: 'ts-jest',
   testEnvironment: 'jsdom',
   modulePaths: [
     '<rootDir>',
   ],
+  transform: {
+    ".ts": ['ts-jest', {
+      useESM: true,
+      /** 
+       * ts-jest preset currently has an issue with the 'verbatimModuleSyntax' typescript option:
+       * This whole transform bit should be removed once this is resolved:
+       * https://github.com/kulshekhar/ts-jest/issues/4081
+       */
+      isolatedModules: true,
+    }]
+  },
   // Module Name settings required to make chalk work with jest
   moduleNameMapper: {
     '#(.*)': '<rootDir>/node_modules/$1',
