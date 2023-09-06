7edab07ac251599c402cb1c91a06fca0f3407164
leeyi45 <leeyi45@gmail.com>
2023-09-06 18:16:41 +0800

    Update scabble documentation

--- src/bundles/scrabble/functions.ts
+++ src/bundles/scrabble/functions.ts
@@ -1,10 +1,3 @@
-/**
- * The `scrabble` Source Module provides the allowable
- * words in Scrabble in a list and in an array, according to
- * https://github.com/benjamincrom/scrabble/blob/master/scrabble/dictionary.json
- * @module scrabble
- */
-
 import type { List } from './types';
 
 /**
--- src/bundles/scrabble/index.ts
+++ src/bundles/scrabble/index.ts
@@ -1,5 +1,8 @@
 /**
- * Scrabble words for Source Academy
+ * The `scrabble` Source Module provides the allowable
+ * words in Scrabble in a list and in an array, according to
+ * https://github.com/benjamincrom/scrabble/blob/master/scrabble/dictionary.json
+ * @module scrabble
  * @author Martin Henz
  */
 export {
