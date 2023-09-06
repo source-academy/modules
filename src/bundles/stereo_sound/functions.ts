303f8b8dd9830cc1b967fd44c1b6dc27c802c9b9
leeyi45 <leeyi45@gmail.com>
2023-09-06 18:17:03 +0800

    Update stereo_sound documentation

--- src/bundles/stereo_sound/functions.ts
+++ src/bundles/stereo_sound/functions.ts
@@ -1,17 +1,3 @@
-/**
- *
- * The stereo sounds library build on the sounds library by accommodating stereo sounds.
- * Within this library, all sounds are represented in stereo, with two waves, left and right.
- *
- * A Stereo Sound is a `pair(pair(left_wave, right_wave), duration)` where duration is the length of the sound in seconds.
- * The constructor `make_stereo_sound` and accessors `get_left_wave`, `get_right_wave`, and `get_duration` are provided.
- * The `make_sound` constructor from sounds is syntatic sugar for `make_stereo_sounds` with equal waves.
- *
- * @module stereo_sound
- * @author Koh Shang Hui
- * @author Samyukta Sounderraman
- */
-
 /* eslint-disable new-cap, @typescript-eslint/naming-convention */
 import {
   accumulate,
