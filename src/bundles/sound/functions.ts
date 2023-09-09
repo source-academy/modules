4a963ad9ec62a45b5532158b3f024db454d5c9d1
Martin Henz <henz@comp.nus.edu.sg>
2023-09-09 09:56:04 +0800

    fix recording (#236)

--- src/bundles/sound/functions.ts
+++ src/bundles/sound/functions.ts
@@ -127,7 +127,7 @@ const recording_signal_ms = 100;
 const pre_recording_signal_pause_ms = 200;
 
 function play_recording_signal() {
-  play(sine_sound(1200, recording_signal_ms / 1000));
+  play_concurrently(sine_sound(1200, recording_signal_ms / 1000));
 }
 
 // eslint-disable-next-line @typescript-eslint/no-shadow
