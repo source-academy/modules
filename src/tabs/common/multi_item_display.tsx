225509cdd48d91ca746923743cfc8a509a690fba
leeyi45 <leeyi45@gmail.com>
2023-09-11 01:57:07 +0800

    Update multi item canvas to include item selector

--- src/tabs/common/multi_item_display.tsx
+++ src/tabs/common/multi_item_display.tsx
@@ -1,9 +1,22 @@
-import { Button } from '@blueprintjs/core';
+import { Button, EditableText } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
-import React from 'react';
+import { clamp } from 'lodash';
+import { useState } from 'react';
 
-const MultiItemDisplay = (props: { elements: JSX.Element[] }) => {
-  const [currentStep, setCurrentStep] = React.useState(0);
+type MultiItemDisplayProps = {
+  elements: JSX.Element[]
+};
+
+const MultiItemDisplay = ({ elements }: MultiItemDisplayProps) => {
+  // The actual index of the currently selected element
+  const [currentStep, setCurrentStep] = useState(0);
+
+  // State for managing the value of the editor
+  const [stepEditorValue, setStepEditorValue] = useState('1');
+  const [stepEditorFocused, setStepEditorFocused] = useState(false);
+
+  const resetStepEditor = () => setStepEditorValue((currentStep + 1).toString());
+  const elementsDigitCount = Math.floor(Math.log10(Math.max(1, elements.length))) + 1;
 
   return (
     <div
@@ -15,50 +28,101 @@ const MultiItemDisplay = (props: { elements: JSX.Element[] }) => {
         height: '100%',
       }}
     >
-      {props.elements.length > 1
-        ? (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'row',
+          position: 'relative',
+          marginBottom: 10,
+        }}
+      >
+        <Button
+          style={{
+            position: 'absolute',
+            left: 0,
+          }}
+          large
+          outlined
+          icon={IconNames.ARROW_LEFT}
+          onClick={() => {
+            setCurrentStep(currentStep - 1);
+            setStepEditorValue(currentStep.toString());
+          }}
+          disabled={currentStep === 0}
+        >
+          Previous
+        </Button>
+        <h3 className="bp3-text-large">
           <div
             style={{
               display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
               flexDirection: 'row',
-              position: 'relative',
-              marginBottom: 10,
-            }}
-          >
-            <Button
-              style={{
-                position: 'absolute',
-                left: 0,
-              }}
-              large
-              outlined
-              icon={IconNames.ARROW_LEFT}
-              onClick={() => setCurrentStep(currentStep - 1)}
-              disabled={currentStep === 0}
-            >
-            Previous
-            </Button>
-            <h3 className="bp3-text-large">
-            Call {currentStep + 1}/{props.elements.length}
-            </h3>
-            <Button
-              style={{
-                position: 'absolute',
-                right: 0,
-              }}
-              large
-              outlined
-              icon={IconNames.ARROW_RIGHT}
-              onClick={() => setCurrentStep(currentStep + 1)}
-              disabled={currentStep === props.elements.length - 1}
-            >
-            Next
-            </Button>
+              justifyContent: 'space-around',
+            }}>
+            Call&nbsp;
+            {/* When the text box is focused, it shows a little up and down bar, which needs a little bit
+              * more space to be shown
+              */}
+            <div style={{ width: `${stepEditorFocused ? elementsDigitCount + 2 : elementsDigitCount}ch` }}>
+              <EditableText
+                value={stepEditorValue}
+                disabled={elements.length === 1}
+                placeholder={undefined}
+                type="number"
+                onChange={(newValue) => {
+                  // Disallow non numeric inputs
+                  if (newValue && !/^[0-9]+$/u.test(newValue)) return;
+
+                  // Disallow numbers that have too many digits
+                  if (newValue.length > elementsDigitCount) return;
+                  setStepEditorValue(newValue);
+                }}
+                onConfirm={(value) => {
+                  if (value) {
+                    const newStep = Number.parseFloat(value);
+                    const clampedStep = clamp(newStep, 1, elements.length);
+                    setCurrentStep(clampedStep - 1);
+                    setStepEditorFocused(false);
+                    setStepEditorValue(clampedStep.toString());
+                    return;
+                  }
+
+                  // If the input element is blank
+                  // then reset it
+                  resetStepEditor();
+
+                  // Indicate that the editor is no longer focused
+                  setStepEditorFocused(false);
+                }}
+                onCancel={() => {
+                  resetStepEditor();
+                  setStepEditorFocused(false);
+                }}
+                onEdit={() => setStepEditorFocused(true)}
+              />
+            </div>
+            {stepEditorFocused && <>&nbsp;</>}/{elements.length}
           </div>
-        )
-        : null}
+        </h3>
+        <Button
+          style={{
+            position: 'absolute',
+            right: 0,
+          }}
+          large
+          outlined
+          icon={IconNames.ARROW_RIGHT}
+          onClick={() => {
+            setCurrentStep(currentStep + 1);
+            setStepEditorValue((currentStep + 2).toString());
+          }}
+          disabled={currentStep === elements.length - 1}
+        >
+          Next
+        </Button>
+      </div>
       <div
         style={{
           width: '100%',
@@ -69,7 +133,7 @@ const MultiItemDisplay = (props: { elements: JSX.Element[] }) => {
           justifyContent: 'center',
         }}
       >
-        {props.elements[currentStep]}
+        {elements[currentStep]}
       </div>
     </div>
   );
