c2ad97d45493057f1cb1e9245110ae097e7fa304
leeyi45 <leeyi45@gmail.com>
2023-09-11 01:58:56 +0800

    Update animation canvas to include error handling

--- src/tabs/common/animation_canvas.tsx
+++ src/tabs/common/animation_canvas.tsx
@@ -299,19 +299,41 @@ AnimCanvasState
       <div style={{
         display: 'flex',
         flexDirection: 'column',
+        alignItems: 'center',
       }}>
         <div
           style={{
             display: 'flex',
-            alignContent: 'center',
+            alignItems: 'center',
             justifyContent: 'center',
           }}
         >
           {this.state.errored
             ? (
-              <div>
-                <h3>An error occurred while running your animation!</h3>
-                Here's the details: <code>
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}>
+                <div style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                  <Icon icon={IconNames.WARNING_SIGN} size={90} />
+                  <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    marginBottom: 20,
+                  }}>
+                    <h3>An error occurred while running your animation!</h3>
+                    <p style={{ justifySelf: 'flex-end' }}>Here's the details:</p>
+                  </div>
+                </div>
+                <code style={{
+                  color: 'red',
+                }}>
                   {this.state.errored.toString()}
                 </code>
               </div>)
