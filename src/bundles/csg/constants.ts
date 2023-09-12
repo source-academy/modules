b7b8d56a4419dc13d8c24667dfbf864b2312ad90
leeyi45 <leeyi45@gmail.com>
2023-09-12 17:12:41 +0800

    Resolve further merge conflicts

--- src/bundles/csg/constants.ts
+++ src/bundles/csg/constants.ts
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-/* [Imports] */
-import { IconSize } from '@blueprintjs/core';
-
-/* [Exports] */
-
-//NOTE Silver is in here to avoid circular dependencies, instead of in
-// functions.ts with the other colour strings
-export const SILVER: string = '#AAAAAA';
-export const DEFAULT_COLOR: string = SILVER;
-
-// Values extracted from the styling of the frontend
-export const SA_TAB_BUTTON_WIDTH: string = '40px';
-export const SA_TAB_ICON_SIZE: number = IconSize.LARGE;
-
-export const BP_TOOLTIP_PADDING: string = '10px 12px';
-export const BP_TAB_BUTTON_MARGIN: string = '20px';
-export const BP_TAB_PANEL_MARGIN: string = '20px';
-export const BP_BORDER_RADIUS: string = '3px';
-export const STANDARD_MARGIN: string = '10px';
-
-export const BP_TEXT_COLOR: string = '#F5F8FA';
-export const BP_TOOLTIP_BACKGROUND_COLOR: string = '#E1E8ED';
-export const BP_ICON_COLOR: string = '#A7B6C2';
-export const ACE_GUTTER_TEXT_COLOR: string = '#8091A0';
-export const ACE_GUTTER_BACKGROUND_COLOR: string = '#34495E';
-export const BP_TOOLTIP_TEXT_COLOR: string = '#394B59';
-
-// Renderer grid constants
-export const MAIN_TICKS: number = 1;
-export const SUB_TICKS: number = MAIN_TICKS / 4;
-export const GRID_PADDING: number = MAIN_TICKS;
-export const ROUND_UP_INTERVAL: number = MAIN_TICKS;
-
-// Controls zoom constants
-export const ZOOM_TICK_SCALE: number = 0.1;
-
-// Controls rotation constants
-export const ROTATION_SPEED: number = 0.0015;
-
-// Controls pan constants
-export const X_FACTOR: number = 1;
-export const Y_FACTOR: number = 0.75;
-=======
 /* [Exports] */
 
 // Renderer default colour. Bright aquamarine makes bugs easier to spot
@@ -62,4 +18,3 @@ export const ROTATION_SPEED: number = 0.0015;
 // Controls pan constants
 export const X_FACTOR: number = 1;
 export const Y_FACTOR: number = 0.75;
->>>>>>> origin/master
--- src/bundles/csg/index.ts
+++ src/bundles/csg/index.ts
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-/* [Imports] */
-import context from 'js-slang/context';
-import { Core } from './core.js';
-import { CsgModuleState } from './utilities.js';
-
-/* [Main] */
-let moduleState = new CsgModuleState();
-
-context.moduleContexts.csg.state = moduleState;
-// We initialise Core for the first time over on the bundles' end here
-Core.initialize(moduleState);
-
-/* [Exports] */
-export {
-  black,
-  blue,
-  bounding_box,
-  cone,
-  crimson,
-  cube,
-  cyan,
-  cylinder,
-  geodesic_sphere,
-  gray,
-  green,
-  group,
-  intersect,
-  is_group,
-  is_shape,
-  lime,
-  navy,
-  orange,
-  pink,
-  prism,
-  purple,
-  pyramid,
-  render,
-  render_axes,
-  render_grid,
-  render_grid_axes,
-  rgb,
-  rose,
-  rotate,
-  rounded_cube,
-  rounded_cylinder,
-  scale,
-  shape_to_stl,
-  silver,
-  sphere,
-  star,
-  subtract,
-  teal,
-  torus,
-  translate,
-  union,
-  white,
-  yellow,
-} from './functions';
-=======
 /* [Imports] */
 import context from 'js-slang/context';
 import { Core } from './core.js';
@@ -132,4 +72,3 @@ export {
   render_axes,
   render_grid_axes,
 } from './functions';
->>>>>>> origin/master
