d3438797bb63e959a351fcc1abcf7dacd9982a05
leeyi45 <leeyi45@gmail.com>
2023-09-18 00:34:18 +0800

    Update CI to allow jest to use color
    (cherry picked from commit 7ea5fa670642fbb4263d4c5de54b6cc86bd3be62)

--- scripts/src/testing.ts
+++ scripts/src/testing.ts
@@ -16,6 +16,7 @@ const testCommand = new Command('test')
   .option('-v, --verbose', 'Run Jest in verbose mode', false)
   .option('-w, --watch', 'Run jest in watch mode', false)
   .option('-u, --updateSnapshot', 'Update snapshots', false)
+  .allowUnknownOption()
   .action(async (patterns: string[] | null, { srcDir, ...others }: TestCommandOptions) => {
     // A wrapper around running jest to convert windows paths to posix paths
     // since jest doesn't know how to match windows paths
