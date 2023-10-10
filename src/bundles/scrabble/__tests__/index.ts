fc7394d858efecf67e4f7d81c30db6dfa6f44456
Martin Henz <henz@comp.nus.edu.sg>
2023-10-11 02:15:09 +0800

    scrabble: correcting test case (#253)
    * adding all scrabble words
    
    * scrabble_words contains all scrabble words
    
    * linting{
    
    * comment in test case corrected
    
    ---------
    
    Co-authored-by: Richard Dominick <34370238+RichDom2185@users.noreply.github.com>

--- src/bundles/scrabble/__tests__/index.ts
+++ src/bundles/scrabble/__tests__/index.ts
@@ -2,7 +2,7 @@ import { scrabble_words } from '../functions';
 
 // Test functions
 
-test('get the first word in the array', () => {
+test('get the word in the array at index 12', () => {
   expect(scrabble_words[12])
     .toBe('aardwolves');
 });
