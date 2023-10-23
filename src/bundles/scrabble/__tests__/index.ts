82dd645b1b226835d68ed0425fe641d017305e39
Martin Henz <henz@comp.nus.edu.sg>
2023-10-23 22:18:03 +0800

    adding scrabble_letters (#256)
    * adding scrabble_letters
    
    * single quote
    
    * linting

--- src/bundles/scrabble/__tests__/index.ts
+++ src/bundles/scrabble/__tests__/index.ts
@@ -1,9 +1,14 @@
-import { scrabble_words } from '../functions';
+import { scrabble_words, scrabble_letters } from '../functions';
 
 // Test functions
 
-test('get the word in the array at index 12', () => {
+test('get the word in the scrabble_words array at index 12', () => {
   expect(scrabble_words[12])
     .toBe('aardwolves');
+});
+
+test('get the word in the scrabble_letters array at index 100000', () => {
+  expect(scrabble_letters[100000][0])
+    .toBe('n');
 });
 
