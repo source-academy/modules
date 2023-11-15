cd792e5ef948b0ff6a98193eda68c37fe2d4ccd1
joel chan <joelchanzhiyang@gmail.com>
2023-11-15 08:46:34 +0000

    Formatting

--- src/bundles/robot_simulation/simulation/controllers/physics/helpers.ts
+++ src/bundles/robot_simulation/simulation/controllers/physics/helpers.ts
@@ -4,4 +4,4 @@ export const quat = ({ x, y, z, w }) => new Quaternion(x, y, z, w);
 export const vec3 = ({ x, y, z }) => new Vector3(x, y, z);
 export const euler = ({ x, y, z }) => new Euler(x, y, z);
 
-export type Vector = { x: number, y:number, z:number };
+export type Vector = { x: number; y: number; z: number };
