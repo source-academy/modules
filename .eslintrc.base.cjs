eca5f71d66092f7b2f9e556275a9c76d52a774e7
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2024-02-18 07:58:24 +0800

    Reformat base ESLint config file

--- .eslintrc.base.cjs
+++ .eslintrc.base.cjs
@@ -2,7 +2,6 @@
 let todoTreeKeywordsWarning = ['TODO', 'TODOS', 'TODO WIP', 'FIXME', 'WIP'];
 let todoTreeKeywordsAll = [...todoTreeKeywordsWarning, 'NOTE', 'NOTES', 'LIST'];
 
-
 /** @type {import('eslint').Linter.Config} */
 const eslintConfig = {
   /** @see https://eslint.org/docs/latest/rules/ */
@@ -10,77 +9,74 @@ const eslintConfig = {
 
   env: {
     node: true,
-    es2022: true,
+    es2022: true
   },
 
   /** @type {Partial<import('eslint/rules').ESLintRules>} */
   rules: {
     'array-callback-return': [
-      "warn",
+      'warn',
       {
         // @ts-ignore incorrect type definition
-        checkForEach: true,
-      },
+        checkForEach: true
+      }
     ],
-    'for-direction': "warn",
-    'getter-return': "warn",
-    'no-async-promise-executor': "warn",
+    'for-direction': 'warn',
+    'getter-return': 'warn',
+    'no-async-promise-executor': 'warn',
     'no-await-in-loop': 1,
-    'no-class-assign': "warn",
-    'no-compare-neg-zero': "warn",
+    'no-class-assign': 'warn',
+    'no-compare-neg-zero': 'warn',
     'no-cond-assign': [
-      "warn",
-      'always', // Was "except-parens"
+      'warn',
+      'always' // Was "except-parens"
     ],
     // "no-const-assign": 2,
-    'no-constant-condition': [
-      "warn",
-      { checkLoops: false },
-    ],
+    'no-constant-condition': ['warn', { checkLoops: false }],
     'no-constructor-return': 1,
-    'no-control-regex': "warn",
-    'no-debugger': "warn",
+    'no-control-regex': 'warn',
+    'no-debugger': 'warn',
     // "no-dupe-args": 2,
-    'no-dupe-class-members': "warn",
-    'no-dupe-else-if': "warn",
-    'no-dupe-keys': "warn",
-    'no-duplicate-case': "warn",
+    'no-dupe-class-members': 'warn',
+    'no-dupe-else-if': 'warn',
+    'no-dupe-keys': 'warn',
+    'no-duplicate-case': 'warn',
     'no-duplicate-imports': 1,
-    'no-empty-character-class': "warn",
-    'no-empty-pattern': "warn",
-    'no-ex-assign': "warn",
-    'no-fallthrough': "warn",
-    'no-func-assign': "warn",
+    'no-empty-character-class': 'warn',
+    'no-empty-pattern': 'warn',
+    'no-ex-assign': 'warn',
+    'no-fallthrough': 'warn',
+    'no-func-assign': 'warn',
     // "no-import-assign": 2,
     'no-inner-declarations': 0, // Was 2
     // "no-invalid-regexp": 2,
     'no-irregular-whitespace': [
-      "warn",
+      'warn',
       {
         // skipStrings: true,
         // skipComments: false,
         // skipRegExps: false,
-        skipTemplates: true,
-      },
+        skipTemplates: true
+      }
     ],
-    'no-loss-of-precision': "warn",
-    'no-misleading-character-class': "warn",
+    'no-loss-of-precision': 'warn',
+    'no-misleading-character-class': 'warn',
     // "no-new-symbol": 2,
     // "no-obj-calls": 2,
     'no-promise-executor-return': 1,
     // "no-prototype-builtins": 2,
-    'no-self-assign': "warn",
+    'no-self-assign': 'warn',
     'no-self-compare': 1,
-    'no-setter-return': "warn",
-    'no-sparse-arrays': "warn",
+    'no-setter-return': 'warn',
+    'no-sparse-arrays': 'warn',
     'no-template-curly-in-string': 1,
     // "no-this-before-super": 2,
     'no-undef': [2, { typeof: true }],
-    'no-unexpected-multiline': "warn",
+    'no-unexpected-multiline': 'warn',
     'no-unmodified-loop-condition': 1,
-    'no-unreachable': "warn",
+    'no-unreachable': 'warn',
     'no-unreachable-loop': 1,
-    'no-unsafe-finally': "warn",
+    'no-unsafe-finally': 'warn',
     // FIXME: Investigate type error
     // 'no-unsafe-negation': [
     //   "warn",
@@ -89,18 +85,18 @@ const eslintConfig = {
     'no-unsafe-optional-chaining': [2, { disallowArithmeticOperators: true }],
     'no-unused-private-class-members': 1,
     'no-unused-vars': [
-      "warn",
+      'warn',
       {
         argsIgnorePattern: '^_',
         caughtErrors: 'all', // Was "none"
-        caughtErrorsIgnorePattern: '^_',
-      },
+        caughtErrorsIgnorePattern: '^_'
+      }
     ],
     'no-use-before-define': [
       1,
       {
-        functions: false,
-      },
+        functions: false
+      }
     ],
     'no-useless-backreference': 1,
     'require-await': 1,
@@ -113,10 +109,7 @@ const eslintConfig = {
     //     enforceForIndexOf: true,
     //   },
     // ],
-    'valid-typeof': [
-      "warn",
-      { requireStringLiterals: true },
-    ],
+    'valid-typeof': ['warn', { requireStringLiterals: true }],
 
     // [Suggestions]
     'accessor-pairs': 1,
@@ -130,7 +123,7 @@ const eslintConfig = {
     'consistent-this': 1,
     curly: [
       1,
-      'multi-line', // Was "all"
+      'multi-line' // Was "all"
     ],
     // "default-case": 0,
     'default-case-last': 1,
@@ -141,29 +134,26 @@ const eslintConfig = {
       1,
       'always', // Same
       {
-        considerPropertyDescriptor: true,
-      },
+        considerPropertyDescriptor: true
+      }
     ],
     // "func-names": 0,
     'func-style': [
       1,
-      'declaration', // Was "expression"
+      'declaration' // Was "expression"
     ],
     'grouped-accessor-pairs': [
       1,
-      'getBeforeSet', // Was "anyOrder"
+      'getBeforeSet' // Was "anyOrder"
     ],
     'new-cap': 1,
     'no-array-constructor': 1,
     'no-bitwise': 1,
     'no-caller': 2,
-    'no-case-declarations': "warn",
+    'no-case-declarations': 'warn',
     'no-confusing-arrow': 1,
     'no-else-return': [1, { allowElseIf: false }],
-    'no-empty': [
-      "warn",
-      { allowEmptyCatch: true },
-    ],
+    'no-empty': ['warn', { allowEmptyCatch: true }],
     'no-empty-function': 1,
     'no-eq-null': 1,
     'no-extend-native': 1,
@@ -174,7 +164,7 @@ const eslintConfig = {
     //   { enforceForLogicalOperands: true },
     // ],
     'no-extra-label': 1,
-    'no-extra-semi': "warn",
+    'no-extra-semi': 'warn',
     'no-floating-decimal': 1,
     'no-implicit-coercion': 1,
     'no-implicit-globals': [1, { lexicalBindings: true }],
@@ -194,8 +184,8 @@ const eslintConfig = {
     'no-new-wrappers': 1,
     'no-octal-escape': 2,
     'no-proto': 1,
-    'no-redeclare': "warn",
-    'no-regex-spaces': "warn",
+    'no-redeclare': 'warn',
+    'no-regex-spaces': 'warn',
     'no-return-assign': 1,
     'no-return-await': 1,
     'no-script-url': 1,
@@ -203,35 +193,35 @@ const eslintConfig = {
     'no-shadow': [
       1,
       {
-        builtinGlobals: true,
+        builtinGlobals: true
         // hoist: "functions"
-      },
+      }
     ],
     'no-throw-literal': 1,
     'no-undef-init': 1,
     'no-unneeded-ternary': [
       1,
       {
-        defaultAssignment: false, // Use || or ?? instead
-      },
+        defaultAssignment: false // Use || or ?? instead
+      }
     ],
     'no-unused-expressions': 1,
-    'no-unused-labels': "warn",
+    'no-unused-labels': 'warn',
     'no-useless-call': 1,
-    'no-useless-catch': "warn",
+    'no-useless-catch': 'warn',
     'no-useless-computed-key': [1, { enforceForClassMembers: true }],
     'no-useless-concat': 1,
     'no-useless-constructor': 1,
-    'no-useless-escape': "warn",
+    'no-useless-escape': 'warn',
     'no-useless-rename': 1,
     'no-useless-return': 1,
     'no-var': 1,
     'no-void': 1,
     'no-warning-comments': [1, { terms: todoTreeKeywordsWarning }],
-    'object-shorthand': ["warn", 'always', { avoidExplicitReturnArrows: true }],
+    'object-shorthand': ['warn', 'always', { avoidExplicitReturnArrows: true }],
     'one-var': [
       1,
-      'never', // Was "always"
+      'never' // Was "always"
     ],
     'one-var-declaration-per-line': 1,
     'operator-assignment': 1,
@@ -253,25 +243,25 @@ const eslintConfig = {
       {
         keywords: true,
         // unnecessary: true,
-        numbers: true,
-      },
+        numbers: true
+      }
     ],
     radix: [
       1,
-      'as-needed', // Was "always"
+      'as-needed' // Was "always"
     ],
     'require-unicode-regexp': 1,
-    'require-yield': "warn",
+    'require-yield': 'warn',
     'spaced-comment': [
       1,
       'always', // Same
-      { markers: todoTreeKeywordsAll },
+      { markers: todoTreeKeywordsAll }
     ],
     '@typescript-eslint/consistent-type-imports': [
       1,
       {
         fixStyle: 'inline-type-imports',
-        prefer: 'type-imports',
+        prefer: 'type-imports'
       }
     ],
     // "strict": 0, // Don't force, though rule configs assume strict errors
@@ -282,12 +272,12 @@ const eslintConfig = {
     // [Layout & Formatting]
     'array-bracket-newline': [
       1,
-      'consistent', // Was "multiline". Limitation: No consistent + multiline
+      'consistent' // Was "multiline". Limitation: No consistent + multiline
     ],
     'array-bracket-spacing': 1,
     'array-element-newline': [
       1,
-      'consistent', // Was "always". Limitation: No consistent + multiline
+      'consistent' // Was "always". Limitation: No consistent + multiline
     ],
     'arrow-parens': 1,
     'arrow-spacing': 1,
@@ -299,33 +289,33 @@ const eslintConfig = {
     'computed-property-spacing': 1,
     'dot-location': [
       1,
-      'property', // Was "object"
+      'property' // Was "object"
     ],
-    'eol-last': 1,
+    'eol-last': 'error',
     'func-call-spacing': 1,
     'function-call-argument-newline': [
       1,
-      'consistent', // Was "always". Limitation: No consistent + multiline
+      'consistent' // Was "always". Limitation: No consistent + multiline
     ],
     'function-paren-newline': [
       1,
-      'consistent', // Was "multiline". Limitation: No consistent + multiline
+      'consistent' // Was "multiline". Limitation: No consistent + multiline
     ],
     'generator-star-spacing': [
       1,
-      'after', // Was "before"
+      'after' // Was "before"
     ],
     'implicit-arrow-linebreak': 1,
     indent: [
       1,
       'tab', // Was 4
       {
-        SwitchCase: 1, // Was 0
+        SwitchCase: 1 // Was 0
         // StaticBlock: {
         // 	body: 1
         // },
         // offsetTernaryExpressions: false,
-      },
+      }
     ],
     'jsx-quotes': 1,
     'key-spacing': 1,
@@ -341,25 +331,25 @@ const eslintConfig = {
     'max-statements-per-line': 1,
     'multiline-ternary': [
       1,
-      'always-multiline', // Was "always"
+      'always-multiline' // Was "always"
     ],
     'new-parens': 1,
     'newline-per-chained-call': [
       1,
       {
-        ignoreChainWithDepth: 1, // warn
-      },
+        ignoreChainWithDepth: 1 // warn
+      }
     ],
     // "no-extra-parens": 0, // Limitation: No exception for ternary conditions
-    'no-mixed-spaces-and-tabs': "warn",
+    'no-mixed-spaces-and-tabs': 'warn',
     'no-multi-spaces': 1,
     'no-multiple-empty-lines': [
       1,
       {
         max: 3, // Was 2
         maxEOF: 0,
-        maxBOF: 0,
-      },
+        maxBOF: 0
+      }
     ],
     // "no-tabs": 0, // Limitation: allowIndentationTabs doesn't allow partial tabs from commenting a block with deeper indentation
     'no-trailing-spaces': 1,
@@ -369,30 +359,30 @@ const eslintConfig = {
       1,
       {
         multiline: true,
-        consistent: true, // Same. Only default if no object option
-      },
+        consistent: true // Same. Only default if no object option
+      }
     ],
     'object-curly-spacing': [
       1,
-      'always', // Was "never"
+      'always' // Was "never"
     ],
     'object-property-newline': 1,
     'operator-linebreak': [
       1,
-      'before', // Was "after"
+      'before' // Was "after"
     ],
     'padded-blocks': [
       1,
-      'never', // Was "always"
+      'never' // Was "always"
     ],
     // "padding-line-between-statements": 0,
     quotes: [
       1,
       'double', // Same
       {
-        avoidEscape: true,
+        avoidEscape: true
         // allowTemplateLiterals: false
-      },
+      }
     ],
     'rest-spread-spacing': 1,
     semi: 1,
@@ -404,8 +394,8 @@ const eslintConfig = {
       {
         anonymous: 'never',
         named: 'never',
-        asyncArrow: 'always',
-      },
+        asyncArrow: 'always'
+      }
     ],
     'space-in-parens': 1,
     'space-infix-ops': 1,
@@ -417,11 +407,11 @@ const eslintConfig = {
     'wrap-iife': [
       1,
       'inside', // Was "outside"
-      { functionPrototypeMethods: true },
+      { functionPrototypeMethods: true }
     ],
     // "wrap-regex": 0,
-    'yield-star-spacing': 1,
-  },
+    'yield-star-spacing': 1
+  }
 };
 
 module.exports = eslintConfig;
