e7d3fe2219b41ad8abe7122989f19a76729b176b
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2024-03-01 13:09:27 +0800

    Fix missing exit toggle

--- src/bundles/ar/ARComponent.tsx
+++ src/bundles/ar/ARComponent.tsx
@@ -45,6 +45,8 @@ function ButtonComponent(props: ARState) {
   return (
     <div style={{ height: '50vh' }}>
       <ARButton
+        enterOnly
+        style={{}}
         sessionInit={{
           requiredFeatures: ['hit-test'],
           optionalFeatures: ['dom-overlay'],
@@ -64,61 +66,90 @@ function Overlay() {
   return (
     <div
       style={{
+        height: 150,
         display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
+        flexDirection: 'column',
         position: 'absolute',
         bottom: 0,
         left: 0,
         right: 0,
-        padding: '0px 20px 60px 20px',
+        padding: '0px 20px 20px 20px',
       }}
     >
-      <button
-        id="recalibrate-toggle"
+      <div
         style={{
-          background: '#212121',
-          color: '#fafafa',
-          borderRadius: 30,
-          fontSize: 20,
-          fontWeight: 500,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          padding: '0px 0px 20px 0px',
         }}
       >
-        ↻
-      </button>
-      <button
-        id="left-toggle"
+        <button
+          id="recalibrate-toggle"
+          style={{
+            background: '#212121',
+            color: '#fafafa',
+            borderRadius: 30,
+            fontSize: 20,
+            fontWeight: 500,
+          }}
+        >
+          ↻
+        </button>
+        <button
+          id="left-toggle"
+          style={{
+            display: 'none',
+            width: 100,
+            background: '#fafafa',
+            color: '#212121',
+            borderRadius: 30,
+            padding: '15px',
+          }}
+        />
+        <button
+          id="center-toggle"
+          style={{
+            display: 'none',
+            width: 100,
+            background: '#fafafa',
+            color: '#212121',
+            borderRadius: 30,
+            padding: '15px',
+          }}
+        />
+        <button
+          id="right-toggle"
+          style={{
+            display: 'none',
+            width: 100,
+            background: '#fafafa',
+            color: '#212121',
+            borderRadius: 30,
+            padding: '15px',
+          }}
+        />
+      </div>
+      <div
         style={{
-          display: 'none',
-          width: 100,
-          background: '#fafafa',
-          color: '#212121',
-          borderRadius: 30,
-          padding: '15px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
         }}
-      />
-      <button
-        id="center-toggle"
-        style={{
-          display: 'none',
-          width: 100,
-          background: '#fafafa',
-          color: '#212121',
-          borderRadius: 30,
-          padding: '15px',
-        }}
-      />
-      <button
-        id="right-toggle"
-        style={{
-          display: 'none',
-          width: 100,
-          background: '#fafafa',
-          color: '#212121',
-          borderRadius: 30,
-          padding: '15px',
-        }}
-      />
+      >
+        <ARButton
+          style={{
+            width: 100,
+            background: '#c5d5db',
+            color: '#212121',
+            borderRadius: 30,
+            padding: '10px 15px',
+          }}
+          exitOnly
+        >
+          Exit
+        </ARButton>
+      </div>
     </div>
   );
 }
