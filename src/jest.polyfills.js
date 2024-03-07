b8d721252838f7308d37eb093dae60d55dbf7f95
joel chan <joelchanzhiyang@gmail.com>
2024-03-07 02:03:08 +0000

    Add a comment in jest polyfills

--- src/jest.polyfills.js
+++ src/jest.polyfills.js
@@ -1,5 +1,6 @@
 const { TextDecoder, TextEncoder } = require("node:util");
 
+// According to the Jest docs, https://mswjs.io/docs/migrations/1.x-to-2.x#environment
 Object.defineProperties(globalThis, {
   TextDecoder: { value: TextDecoder },
   TextEncoder: { value: TextEncoder },
