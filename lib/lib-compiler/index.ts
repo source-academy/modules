7427aa9888cea727ce9288a9fa04f869e292b05c
Lee Yi <leeyi45@gmail.com>
2025-07-28 01:59:18 +0800

    Add lib-compiler

---
+++ lib/lib-compiler/index.ts
@@ -0,0 +1,32 @@
+import { Command } from '@commander-js/extra-typings';
+import chalk from 'chalk';
+import { build, context, type BuildOptions } from 'esbuild';
+
+/**
+ * Returns a {@link Command} that executes with the provided ESBuild options.
+ */
+export default function getBuildCommand(esbuildOptions: Omit<BuildOptions, 'minify'>) {
+  return new Command()
+    .option('--dev', 'If specified, the built output is not minified for easier debugging')
+    .option('--watch', 'Run esbuild in watch mode')
+    .action(async ({ dev, watch }) => {
+      if (watch) {
+        const buildContext = await context({
+          ...esbuildOptions,
+          minify: !dev,
+          plugins: [{
+            name: 'Watch Plugin',
+            setup({ onEnd }) {
+              onEnd(() => {
+                console.log(chalk.greenBright('Build completed.'));
+              });
+            }
+          }]
+        });
+        console.log(chalk.yellowBright('Running ESBuild in watch mode.'));
+        await buildContext.watch();
+      } else {
+        await build(esbuildOptions);
+      }
+    });
+}
