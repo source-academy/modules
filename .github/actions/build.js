be461853d4227e965865b7d41e6566937818642b
Lee Yi <leeyi45@gmail.com>
2025-08-04 15:04:25 +0800

    Remove the old install-deps action

--- .github/actions/build.js
+++ .github/actions/build.js
@@ -5,7 +5,6 @@ const buildCommand = getBuildCommand({
   bundle: true,
   entryPoints: [
     { in: './src/info/index.ts', out: 'info' },
-    { in: './src/install-deps/index.ts', out: 'install-deps' },
   ],
   format: 'esm',
   outdir: 'dist',
