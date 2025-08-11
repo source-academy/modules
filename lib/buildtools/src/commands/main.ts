c0a61378a27e5a0cb9b6f64481eec09418177ba6
Lee Yi <leeyi45@gmail.com>
2025-08-11 14:20:06 +0800

    Add http server command to buildtools

--- lib/buildtools/src/commands/main.ts
+++ lib/buildtools/src/commands/main.ts
@@ -2,12 +2,14 @@ import { Command } from '@commander-js/extra-typings';
 import { getBuildCommand, getBuildHtmlCommand, getManifestCommand } from './build.js';
 import { getListCommand } from './list.js';
 import { getLintCommand, getLintGlobalCommand, getPrebuildAllCommand, getTscCommand } from './prebuild.js';
+import getHttpServerCommand from './server.js';
 import getTemplateCommand from './template.js';
 import { getTestAllCommand, getTestCommand } from './testing.js';
 
 const commands: (() => Command<any>)[] = [
   getBuildCommand,
   getBuildHtmlCommand,
+  getHttpServerCommand,
   getLintCommand,
   getLintGlobalCommand,
   getListCommand,
