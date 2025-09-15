67ca88c96e06e0950230b7706377f2f74a57c619
DESKTOP-G08HS3B\Lee Yi <leeyi45@gmail.com>
2025-09-15 00:31:20 -0400

    FIx skipped tests displaying empty duration strings

--- lib/vitest-reporter/src/test-reporter.ts
+++ lib/vitest-reporter/src/test-reporter.ts
@@ -5,9 +5,9 @@ import type { Reporter, RunnerTestFile, TestCase, TestModule, TestSuite } from '
 function* formatTestCase(testCase: TestCase) {
   const passed = testCase.ok();
   const diagnostics = testCase.diagnostic();
-  const durationStr = diagnostics ? `${diagnostics.duration.toFixed(0)}ms` : '';
+  const durationStr = diagnostics ? ` <code>${diagnostics.duration.toFixed(0)}ms</code>` : '';
 
-  yield `${passed ? '✅' : '❌'} ${testCase.name} <code>${durationStr}</code>`;
+  yield `${passed ? '✅' : '❌'} ${testCase.name}${durationStr}`;
 
   if (diagnostics?.slow) {
     yield ' ⚠️';
