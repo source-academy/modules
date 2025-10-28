2c995f8d9e07eb3297710a93c31fdfd912148022
Lee Yi <leeyi45@gmail.com>
2025-10-28 18:11:00 -0400

    Add dependency on modules-lib

--- src/bundles/matrix/src/types.ts
+++ src/bundles/matrix/src/types.ts
@@ -1,3 +1,6 @@
+import type { ReplResult } from '@sourceacademy/modules-lib/types';
+import type { Pair } from 'js-slang/dist/stdlib/list';
+
 /**
  * A function that is passed the row and column numbers of the cell that changed
  * and its new value.
@@ -7,15 +10,32 @@ export type CellCallback = (row: number, col: number, newValue: boolean) => void
 /**
  * Representation of a 2D matrix of values
  */
-export interface Matrix {
+export interface Matrix extends ReplResult {
   name: string | undefined;
+
+  /**
+   * Values of the matrix. Should be indexed as values[row][col].
+   */
   values: boolean[][];
+
+  /**
+   * Button labels for the matrix. Should be indexed as labels[row][col].
+   */
   labels: string[][];
+
+  /**
+   * Number of rows
+   */
   readonly rows: number;
+
+  /**
+   * Number of columns
+   */
   readonly cols: number;
+
   toReplString: () => string;
 
-  buttons: [string, () => void][];
+  buttons: Pair<string, () => void>[];
   onCellClick?: CellCallback;
   onColClick?: (col: number, value: boolean) => void;
   onRowClick?: (row: number, value: boolean) => void;
