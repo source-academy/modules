acf4d7b20248603e8cc43455b8f996414a937332
RichDom2185 <RichDom2185@users.noreply.github.com>
2025-11-18 15:28:57 +0000

    deploy: 53a5d3c11f50994ccca3ad2499ed8b12872554e2

--- devdocs/assets/chunks/chunk-QZHKN3VN.0mp7a3E7.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"../app.a9TX7l_7.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
