848429d3f5ef27e18b62d6aec1f7721e51836909
Lee Yi <leeyi45@gmail.com>
2025-12-03 18:38:16 -0500

    Fix incorrect paths referring to test mocks

--- lib/repotools/src/__mocks__/getGitRoot.ts
+++ lib/repotools/src/__mocks__/getGitRoot.ts
@@ -2,7 +2,7 @@ import { join, resolve } from 'path';
 
 const testMocksDir = resolve(import.meta.dirname, '../../../__test_mocks__');
 
-export const bundlesDir = join(testMocksDir, 'bundles');
-export const tabsDir = join(testMocksDir, 'tabs');
+export const bundlesDir = join(testMocksDir, 'src', 'bundles');
+export const tabsDir = join(testMocksDir, 'src', 'tabs');
 export const outDir = '/build';
 export const getGitRoot = () => Promise.resolve(testMocksDir);
