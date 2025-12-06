04a7cfe1b77a458f4aae19f2920d6ec506aeed21
Lee Yi <leeyi45@gmail.com>
2025-12-05 19:33:54 -0500

    Remove obsolete tests

--- .github/actions/src/__tests__/commons.test.ts
+++ .github/actions/src/__tests__/commons.test.ts
@@ -1,5 +1,5 @@
 import * as exec from '@actions/exec';
-import { describe, expect, it, test, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import * as commons from '../commons.js';
 
 vi.mock(import('lodash/memoize.js'), () => ({
@@ -26,67 +26,5 @@ describe(commons.checkDirForChanges, () => {
 
     await expect(commons.checkDirForChanges('/')).resolves.toEqual(false);
     expect(mockedExecOutput).toHaveBeenCalledOnce();
-  });
-});
-
-describe(commons.isPackageRecord, () => {
-  test('no bundleName or tabName property is ok', () => {
-    expect(commons.isPackageRecord({
-      directory: '',
-      name: '',
-      changes: false,
-      needsPlaywright: false
-    })).toEqual(true);
-  });
-
-  test('string bundleName property is ok', () => {
-    expect(commons.isPackageRecord({
-      directory: '',
-      name: '',
-      changes: false,
-      needsPlaywright: false,
-      bundleName: ''
-    })).toEqual(true);
-  });
-
-  test('non-string bundleName property is not ok', () => {
-    expect(commons.isPackageRecord({
-      directory: '',
-      name: '',
-      changes: false,
-      needsPlaywright: false,
-      bundleName: 0
-    })).toEqual(false);
-  });
-
-  test('string tabName property is ok', () => {
-    expect(commons.isPackageRecord({
-      directory: '',
-      name: '',
-      changes: false,
-      needsPlaywright: false,
-      tabName: ''
-    })).toEqual(true);
-  });
-
-  test('non-string tabName property is not ok', () => {
-    expect(commons.isPackageRecord({
-      directory: '',
-      name: '',
-      changes: false,
-      needsPlaywright: false,
-      tabName: 0
-    })).toEqual(false);
-  });
-
-  test('having both bundleName and tabName property is not ok', () => {
-    expect(commons.isPackageRecord({
-      directory: '',
-      name: '',
-      changes: false,
-      needsPlaywright: false,
-      tabName: '',
-      bundleName: ''
-    })).toEqual(false);
   });
 });
