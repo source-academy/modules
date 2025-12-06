b90cd77c2eec3317469066270e7eb5d180dc6b0c
Lee Yi <leeyi45@gmail.com>
2025-12-05 19:32:48 -0500

    Try using Yarn utilities for detecting changes (without lockfile)

--- .github/actions/src/commons.ts
+++ .github/actions/src/commons.ts
@@ -1,65 +1,6 @@
 import { getExecOutput } from '@actions/exec';
 import memoize from 'lodash/memoize.js';
 
-export interface RawPackageRecord {
-  directory: string;
-  hasChanges: boolean;
-  package: {
-    name: string;
-    devDependencies: Record<string, string>;
-    dependencies: Record<string, string>;
-  };
-}
-
-interface BasePackageRecord {
-  /**
-   * Directory within which the `package.json` file was found
-   */
-  directory: string;
-
-  /**
-   * Full scoped package name
-   */
-  name: string;
-  /**
-   * `true` if git detected changes from within the package's subdirectories,
-   * `false` otherwise
-   */
-  changes: boolean;
-  /**
-   * `true` if playwright is present under devDependencies. This means that the package
-   * might need playwright for its tests
-   */
-  needsPlaywright: boolean;
-}
-
-export interface BundlePackageRecord extends BasePackageRecord {
-  bundleName: string;
-}
-
-export interface TabPackageRecord extends BasePackageRecord {
-  tabName: string;
-}
-
-export type PackageRecord = BundlePackageRecord | TabPackageRecord | BasePackageRecord;
-
-export function isPackageRecord(obj: unknown): obj is PackageRecord {
-  if (typeof obj !== 'object' || obj === null) return false;
-
-  if (!('directory' in obj) || typeof obj.directory !== 'string') return false;
-  if (!('name' in obj) || typeof obj.name !== 'string') return false;
-  if (!('changes' in obj) || typeof obj.changes !== 'boolean') return false;
-  if (!('needsPlaywright' in obj) || typeof obj.needsPlaywright !== 'boolean') return false;
-
-  if ('bundleName' in obj) {
-    if ('tabName' in obj || typeof obj.bundleName !== 'string') return false;
-  } else if ('tabName' in obj) {
-    if (typeof obj.tabName !== 'string') return false;
-  }
-
-  return true;
-}
-
 /**
  * Returns `true` if there are changes present in the given directory relative to
  * the master branch\
@@ -71,7 +12,8 @@ export const checkDirForChanges = memoize(async (directory: string) => {
     ['--no-pager', 'diff', '--quiet', 'origin/master', '--', directory],
     {
       failOnStdErr: false,
-      ignoreReturnCode: true
+      ignoreReturnCode: true,
+      silent: true
     }
   );
   return exitCode !== 0;
