650eb57e473d95d742a2d16ee9d9db8417e2cdcc
DESKTOP-G08HS3B\Lee Yi <leeyi45@gmail.com>
2026-04-03 00:30:08 +0800

    Fix twoslash popovers not having styling

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -23,10 +23,10 @@ const vitepressOptions: UserConfig = {
       md.use(directoryTreePlugin);
     },
     codeTransformers: [
-      dirtreeTransformer(),
       transformerTwoslash({
         typesCache: createFileSystemTypesCache()
-      })
+      }),
+      dirtreeTransformer()
     ],
     languages: [dirtreetm]
   },
