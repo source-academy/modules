c209eff1f3ec93d2cd9f66f0725fe6e53e0cea2c
DESKTOP-G08HS3B\Lee Yi <leeyi45@gmail.com>
2026-05-06 00:05:23 +0800

    Remove validator

--- lib/validator/types/bundle.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ModuleBundle } from 'js-slang/dist/modules/moduleTypes';
-
-declare const partialBundle: ModuleBundle;
-export default partialBundle;
--- lib/validator/types/tab.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ModuleSideContent } from "@sourceacademy/modules-lib/types";
-
-type RequireProvider = (s: string) => any;
-
-declare const partialTab: (provider: RequireProvider) => {
-  default: ModuleSideContent
-};
-
-export default partialTab;
