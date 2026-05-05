87b3d7b8c81f8d54fda4ff7dee1a01843def644d
DESKTOP-G08HS3B\Lee Yi <leeyi45@gmail.com>
2026-05-05 23:59:02 +0800

    Refactor docs to have separate page for each linting rule

---
+++ docs/src/lib/lintplugin/2-rules/rules.data.ts
@@ -0,0 +1,17 @@
+import lintPlugin from '@sourceacademy/lint-plugin';
+
+interface RuleInfo {
+  name: string;
+  desc: string;
+}
+
+export default {
+  load() {
+    return Object.entries(lintPlugin.rules).reduce<RuleInfo[]>((res, [name, rule]) => {
+      return [
+        ...res,
+        { name, desc:  rule.meta?.docs?.description ?? '' }
+      ];
+    }, []);
+  }
+};
