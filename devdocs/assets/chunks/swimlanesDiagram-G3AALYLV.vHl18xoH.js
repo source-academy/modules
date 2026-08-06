db2c12dccb7ab32a2632aa731928431243158526
martin-henz <martin-henz@users.noreply.github.com>
2026-08-06 13:42:36 +0000

    deploy: 398b9e42e7c5785a70f8ba09d57dff17e53ef12c

--- devdocs/assets/chunks/swimlanesDiagram-G3AALYLV.vHl18xoH.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U.ohAdV4aG.js";import{_ as a}from"../app.Ci2rcHUj.js";import"./chunk-5VM5RSS4.COWYKrEr.js";import"./chunk-XXDRQBXY.6BmVoJQP.js";import"./chunk-VR4S4FIN.Bz1_KyML.js";import"./chunk-32BRIVSS.CDCtt_Ri.js";import"./channel.BOqUuiEX.js";import"./framework.DuwpaNh0.js";import"./theme.BalXnihw.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
