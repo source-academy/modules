52d7204d1d1cc76d9f3423ccc91d440c3a2499b0
martin-henz <martin-henz@users.noreply.github.com>
2026-08-07 01:10:44 +0000

    deploy: c46f80cc95fd1efde3df70f828de580e9982f1de

--- devdocs/assets/chunks/channel.CbySY_Da.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.Dcp5_6XF.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ devdocs/assets/chunks/swimlanesDiagram-G3AALYLV.ByVj7EOV.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.DAq68Ycn.js";import{_ as a}from"../app.D2JzMtN6.js";import"./chunk-5VM5RSS4.CHG2fUjK.js";import"./chunk-XXDRQBXY.vDFdaVfW.js";import"./chunk-VR4S4FIN.CUujG7eU.js";import"./chunk-32BRIVSS.D-8zhc6v.js";import"./channel.Cjz-nEU7.js";import"./framework.DuwpaNh0.js";import"./theme.DJqXGNkN.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
