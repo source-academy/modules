d6b4cc39760b7693c0b5a9d545a94eba8aed360c
RichDom2185 <RichDom2185@users.noreply.github.com>
2026-09-05 06:30:09 +0000

    deploy: 4703210711b974340dcc5c6f633dbf0fc3dd3dc5

---
+++ devdocs/assets/chunks/channel.DqiqG--x.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.C8Nscelq.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ devdocs/assets/chunks/swimlanesDiagram-VR7AAH4N.BbFg8-n6.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.VmG2jaLc.js";import{_ as a}from"../app.C8Nscelq.js";import"./chunk-5VM5RSS4.CLxN0saz.js";import"./chunk-XXDRQBXY.D-6ozipC.js";import"./chunk-POPQ4Y6H.58uVAJuQ.js";import"./chunk-F27PBJKO.C_TT_akS.js";import"./channel.DqiqG--x.js";import"./framework.DuwpaNh0.js";import"./theme.BhjxgkoG.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
